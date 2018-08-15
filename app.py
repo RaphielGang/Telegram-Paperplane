cc7132f532f3ab0bdf0a97b8400418545fe3fe4c
baalajimaestro <baalajimaestro@computer4u.com>
2018-08-15 20:10:22 +0530

    Fix hastebin ;__;
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- app.py
+++ app.py
@@ -150,8 +150,8 @@ async def pipcheck(event):
 	await a.edit(r)
 @client.on(events.NewMessage(outgoing=True,pattern='.paste'))
 async def haste_paste(event):
+    message=await client.get_messages(event.chat_id)
     await event.reply('`Sending to bin . . .`')
-    text=await client.get_messages(event.chat_id)
     text=str(message[0].message[7:])
     await event.edit('`Sent to bin! Check it here: `' + bin.post(text))
 @client.on(events.NewMessage(pattern='.killme'))
