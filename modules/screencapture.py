f4d914d5d6247bc87cc67e4f480e76701ed771d3
bottest123 <41206183+bottest123@users.noreply.github.com>
2018-10-18 13:47:32 +0530

    New!!

---
+++ modules/screencapture.py
@@ -0,0 +1,31 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import requests
+
+@bot.on(events.NewMessage(pattern=r".screencapture (.*)", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Processing ...")
+    sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}"
+    input_str = event.pattern_match.group(1)
+    response_api = requests.get(sample_url.format(SCREEN_SHOT_LAYER_ACCESS_KEY, input_str), stream=True)
+    temp_file_name = "screenshotlayer.png"
+    with open(temp_file_name, "wb") as fd:
+        for chunk in response_api.iter_content(chunk_size=128):
+            fd.write(chunk)
+    try:
+        await bot.send_file(
+            event.chat_id,
+            temp_file_name,
+            caption=input_str,
+            force_document=True,
+            reply_to=event.message.reply_to_msg_id
+        )
+        await event.delete()
+    except:
+        await event.edit(response_api.text)
+    os.remove(temp_file_name)
