40154ef1e5001dbcfd095e2953fe770be6c29ec0
bottest123 <41206183+bottest123@users.noreply.github.com>
2018-10-18 13:53:23 +0530

    Changes due to new plugins

--- sample_config.py
+++ sample_config.py
@@ -5,6 +5,8 @@ if not __name__.endswith("sample_config"):
     quit(1)
 API_ID="YOUR API ID"
 API_HASH="YOUR API HASH"
+SCREEN_SHOT_LAYER_ACCESS_KEY="get from screenshot layer website google it "           #For using .screencapture commad...please refer readme for getting the key
+OPEN_WEATHER_MAP_APPID="get it from openweather site"        #FOR USING .weather Command
 LOGGER_GROUP="CHAT ID (-ve) OF THE LOG GROUP"
 LOGGER=True    #Incase you want to turn off logging, put this to false
 CONSOLE_LOGGER_VERBOSE=False
