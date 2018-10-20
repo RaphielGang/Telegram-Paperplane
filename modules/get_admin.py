f33cf425daef7fa4a102686a2ad99d0d05424fc6
baalajimaestro <baalajimaestro@computer4u.com>
2018-10-20 18:19:42 +0530

    Add tag admin/get admin option
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- modules/get_admin.py
+++ modules/get_admin.py
@@ -4,24 +4,22 @@
 
 from telethon.tl.types import ChannelParticipantsAdmins, ChatParticipantCreator
 from telethon.errors import ChatAdminRequiredError, InputUserDeactivatedError
-
-
 @bot.on(events.NewMessage(pattern=".get_admin ?(.*)", outgoing=True))
-async def _(event):
+async def get_admin(e):
     if event.fwd_from:
         return
     mentions = "**Admins in this Chat**: \n"
-    input_str = event.pattern_match.group(1)
-    to_write_chat = await event.get_input_chat()
+    choice = e.pattern_match.group(1)
+    to_write_chat = LOGGER_GROUP
     chat = None
     if not input_str:
         chat = to_write_chat
     else:
-        mentions = "Admins in {} channel: \n".format(input_str)
+        mentions = "Admins in channel {}: \n".format(str(e.chat_id))
         try:
             chat = await bot.get_entity(input_str)
-        except ValueError as e:
-            await event.edit(str(e))
+        except ValueError as ea:
+            await e.edit(str(ea))
             return None
     try:
         async for x in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
@@ -29,11 +27,20 @@ async def _(event):
                 mentions += f"\n[{x.first_name}](tg://user?id={x.id}) `{x.id}`"
             else:
                 mentions += f"\n InputUserDeactivatedError `{x.id}`"
-    except ChatAdminRequiredError as e:
-        mentions += " " + str(e) + "\n"
-    await bot.send_message(
+    except ChatAdminRequiredError as ea:
+        mentions += " " + str(ea) + "\n"
+    if choice==1:
+        await bot.send_message(
         to_write_chat,
         mentions,
         reply_to=event.message.reply_to_msg_id
-    )
-    await event.delete()
+        )
+    else:
+      if LOGGER:
+        await e.edit("`Sent admin details to Logs!``")
+        await bot.send_message(
+        to_write_chat,
+        mentions
+        )
+      else:
+        await e.edit("`This feature needs Logging to be enabled!`")
