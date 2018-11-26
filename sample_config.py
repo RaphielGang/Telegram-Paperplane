d85e24fdf11f5a08bd5eb6f63eb4b946a16398e6
baalajimaestro <baalajimaestro@computer4u.com>
2018-11-26 17:26:56 +0530

    Issue Fix: API_ID renamed to API_KEY
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@ if not __name__.endswith("config"):
     import sys
     print("Plox rename and remove this.", file=sys.stderr)
     quit(1)
-API_ID="YOUR API ID"    #get from my.telegram.org
+API_KEY="YOUR API KEY"    #get from my.telegram.org
 API_HASH="YOUR API HASH"  #get from my.telegram.org
 SCREEN_SHOT_LAYER_ACCESS_KEY="get from screenshot layer website google it "           #For using .screencapture commad...please refer readme for getting the key
 OPEN_WEATHER_MAP_APPID="get it from openweather site"        #FOR USING .weather Command
