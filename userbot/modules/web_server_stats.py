09ebfff85db9e924894701163a0dec450240ad30
baalajimaestro <baalajimaestro@computer4u.com>
2018-11-30 01:06:08 +0530

    Dont pull this module
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- userbot/modules/web_server_stats.py
+++ userbot/modules/web_server_stats.py
@@ -8,7 +8,7 @@ from telethon import events
 from userbot import bot,LOGGER,LOGGER_GROUP
 @bot.on(events.NewMessage(outgoing=True,pattern=".webserverstat"))
 @bot.on(events.MessageEdited(outgoing=True,pattern=".webserverstat"))
-async def web_server_stat:
+async def web_server_stat(e):
     result = ""
     if LOGGER:
         result=subprocess.run("sudo systemctl status nginx", stdout=subprocess.PIPE).stdout.decode()
@@ -22,5 +22,7 @@ async def web_server_stat:
         f.write(result)
         f.close()
         await bot.send_file(LOGGER_GROUP, 'sender.txt', reply_to=e.id, caption="`Here is your current status`")
+        r='`' + subprocess.run(['neofetch', '--off', '--color_blocks off', '--bold off', '--cpu_temp', 'C', '--cpu_speed','on','--cpu_cores', 'physical','--stdout'], stdout=subprocess.PIPE).stdout.decode() + '`'
+        await bot.send_message(LOGGER_GROUP,r)
         subprocess.run(['rm', 'output.txt'], stdout=subprocess.PIPE)
         await e.delete()
