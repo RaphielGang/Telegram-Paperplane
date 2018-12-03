0636f771a3fcb4d245b2710bee953d15997e72dd
Rhyse Simpson <skat1@live.com.au>
2018-12-03 14:10:45 +1100

    moar code cleanup. imports from userbot now have spaces after commas

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient, events
 from userbot import bot
 import time
-from userbot import COUNT_MSG,USERS,ISAFK,AFKREASON,LOGGER,LOGGER_GROUP
+from userbot import COUNT_MSG, USERS, ISAFK, AFKREASON, LOGGER, LOGGER_GROUP
 
 
 @bot.on(events.NewMessage(incoming=True))
--- userbot/modules/screencapture.py
+++ userbot/modules/screencapture.py
@@ -6,6 +6,8 @@ import os
 import requests
 from telethon import TelegramClient, events
 from userbot import bot
+
+
 @bot.on(events.NewMessage(pattern=r".screencapture (.*)", outgoing=True))
 async def _(event):
   if not e.text[0].isalpha():
