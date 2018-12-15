58e2be2ba922e37b7b613ad0f9511ad7d8f08444
baalajimaestro <baalajimaestro@computer4u.com>
2018-12-15 19:28:13 +0530

    Fix: Screencapture
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- userbot/modules/screencapture.py
+++ userbot/modules/screencapture.py
@@ -10,16 +10,16 @@ from userbot import bot,SCREEN_SHOT_LAYER_ACCESS_KEY
 
 @bot.on(events.NewMessage(pattern=r".screencapture (.*)", outgoing=True))
 @bot.on(events.MessageEdited(pattern=r".screencapture (.*)", outgoing=True))
-async def _(event):
+async def _(e):
   if not e.text[0].isalpha() and e.text[0] not in ('/','#','@','!'):
-    if event.fwd_from:
+    if e.fwd_from:
         return
     if SCREEN_SHOT_LAYER_ACCESS_KEY is None:
-        await event.edit("Need to get an API key from https://screenshotlayer.com/product \nModule stopping!")
+        await e.edit("Need to get an API key from https://screenshotlayer.com/product \nModule stopping!")
         return
-    await event.edit("Processing ...")
+    await e.edit("Processing ...")
     sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}&fullpage={}&format={}&viewport={}"
-    input_str = event.pattern_match.group(1)
+    input_str = e.pattern_match.group(1)
     response_api = requests.get(sample_url.format(Config.SCREEN_SHOT_LAYER_ACCESS_KEY, input_str, "1", "PNG", "2560x1440"), stream=True)
     contentType = response_api.headers['content-type']
     if "image" in contentType:
@@ -29,15 +29,15 @@ async def _(event):
                 fd.write(chunk)
         try:
             await borg.send_file(
-                event.chat_id,
+                e.chat_id,
                 temp_file_name,
                 caption=input_str,
                 force_document=True,
-                reply_to=event.message.reply_to_msg_id
+                reply_to=e.message.reply_to_msg_id
             )
-            await event.delete()
+            await e.delete()
         except:
-            await event.edit(response_api.text)
+            await e.edit(response_api.text)
         os.remove(temp_file_name)
     else:
-        await event.edit(response_api.text)
+        await e.edit(response_api.text)
