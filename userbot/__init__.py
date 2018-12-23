968415e4541d6993ab5320e97e4317156b2660a9
baalajimaestro <baalajimaestro@computer4u.com>
2018-12-23 15:02:21 +0530

    Fix: Import Error
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -40,7 +40,7 @@ subprocess.run(
     ],
     stdout=subprocess.PIPE,
 )
-if len(sys.argv==1):
+if len(sys.argv)==1:
     print("Type y to go ahead on a bleeding edge build. Else it will remain stable.")
     t=input()
     if t != "y":
