4e8bb53c179fa884cbc848a9183cc7d5ea6c73e2
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-01-31 00:23:06 +0700

    init: autoinstall: it's a hella hoog refactoring
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- init/init_script.sh
+++ init/init_script.sh
@@ -1,71 +1,88 @@
 #!/usr/bin/env dash
 
-clear
-echo "*****Welcome to baalajimaestro's userbot setup*****
+# Welcome Message
+welcome() {
+    echo "*****Welcome to baalajimaestro's userbot setup*****
 This Guided Setup shall help you get your own userbot up and running.
 You might be asked for sudo password several number of times."
+}
 
-if [ "$(lsb_release -is)" = "Ubuntu" ]; then
-    sudo add-apt-repository ppa:deadsnakes/ppa
-fi
+# Package requirements install
+packageinstall() {
+    # add-apt-repository only exists in Ubuntu
+    [ "$(lsb_release -is)" = "Ubuntu" ] && sudo add-apt-repository ppa:deadsnakes/ppa
 
-sudo apt --yes --force-yes upgrade
+    sudo apt --yes --force-yes upgrade
     sudo apt --yes --force-yes install build-essential checkinstall git \
-    libreadline-gplv2-dev libncursesw5-dev libssl-dev wget \
-    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+        libreadline-gplv2-dev libncursesw5-dev libssl-dev wget \
+        libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
     sudo apt --yes --force-yes install python3.7
+}
 
-sudo useradd userbot
+# Create userbot user
+createuser() {
+    sudo useradd --create-home --home /home/userbot userbot
     echo "userbot ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
-clear
-
-cd /tmp || exit
-    git clone https://github.com/baalajimaestro/Telegram-UserBot
-        cd Telegram-UserBot || exit
-
-read -r -p "Press y to go ahead with bleeding builds. Or press any other key for stable " BUILDS
-
-if [ "$BUILDS" != "y" ]; then
-    X=git tag -l | cut -f 1 | tail -n 1
-    git checkout tags/"$X"
     clear
-fi
+}
 
-echo "***Installing Requirements***"
-sudo python3.7 -m pip install -r requirements.txt
+# Clone the required repo
+botclone() {
+    git clone https://github.com/baalajimaestro/Telegram-UserBot
+    cd Telegram-UserBot || exit
+}
+
+# Requirement install function
+reqinstall() {
+    echo "***Installing Requirements***"
+    sudo python3.7 -m pip install -r requirements.txt
     curl -sLo bot https://raw.githubusercontent.com/baalajimaestro/Telegram-UserBot/modular/init/userbot
-clear
+    clear
+}
 
-echo "***Please enter your details***"
-read -r -p "What's your API_ID? " API_KEY
-read -r -p "What's your API_HASH? " API_HASH
-read -r -p "What's your Screenshot Layer API Key? " SCREENSHOT_LAYER_ACCESS_KEY
-read -r -p "Do you need PMPermit Enabled? (y/n) " PMPERMIT
-read -r -p "Do you need Logging Enabled? (y/n) " LOG
-read -r -p "What's your OpenWeatherMap API Key? " OPEN_WEATHER_MAP_APPID
-read -r -p "Do you need a Database Mode? (y/n) " DB
+# Questionaire
+questions() {
+    echo "***Please enter your details***"
+    read -r -p "What's your API_ID? " API_KEY
+    read -r -p "What's your API_HASH? " API_HASH
+    read -r -p "What's your Screenshot Layer API Key? " SCREENSHOT_LAYER_ACCESS_KEY
 
-if [ "$PMPERMIT" = "y" ]; then
-    PM_AUTO_BAN=True
-else
-    PM_AUTO_BAN=False
-fi
+    read -r -p "Do you need PMPermit Enabled? (y/n) " PMPERMIT
+    if [ "$PMPERMIT" = "y" ]; then
+        PM_AUTO_BAN=True
+    else
+        PM_AUTO_BAN=False
+    fi
 
-if [ "$LOG" = "y" ]; then
-    read -r -p "Enter the Log Group ID: " LOGGER_GROUP
-    LOGGER=True
-else
-    LOGGER=False
-    LOGGER_GROUP=0
-fi
+    read -r -p "Do you need Logging Enabled? (y/n) " LOG
+    if [ "$LOG" = "y" ]; then
+        read -r -p "Enter the Log Group ID: " LOGGER_GROUP
+        LOGGER=True
+    else
+        LOGGER=False
+        LOGGER_GROUP=0
+    fi
+    read -r -p "What's your OpenWeatherMap API Key? " OPEN_WEATHER_MAP_APPID
 
-if [ "$DB" = "y" ]; then
-    read -r -p "Enter your DB URL: " DB_URI
-else
-    DB_URI=None
-fi
+    read -r -p "Do you need a Database Mode? (y/n) " DB
+    if [ "$DB" = "y" ]; then
+        read -r -p "Enter your DB URL: " DB_URI
+    else
+        DB_URI=None
+    fi
+}
 
-echo "API_KEY=$API_KEY
+# Checkout to latest tag
+checkout() {
+    read -r -p "Press y to go ahead with bleeding builds. Or press any other key for stable " BUILDS
+    rel=$(git tag -l | cut -f 1 | tail -n 1)
+    [ "$BUILDS" != "y" ] && git checkout tags/"$rel"
+    clear
+}
+
+# Config write function
+writeconfig() {
+    echo "API_KEY=$API_KEY
 API_HASH=$API_HASH
 SCREENSHOT_LAYER_ACCESS_KEY=$SCREENSHOT_LAYER_ACCESS_KEY
 PM_AUTO_BAN=$PM_AUTO_BAN
@@ -73,16 +90,43 @@ LOGGER=$LOGGER
 LOGGER_GROUP=$LOGGER_GROUP
 OPEN_WEATHER_MAP_APPID=$OPEN_WEATHER_MAP_APPID
 DB_URI=$DB_URI" >> config.env
+}
 
-python3.7 -m userbot test
-sudo mv bot /etc/systemd/system/userbot.service
-sudo chown -R userbot /tmp/Telegram-UserBot
-sudo chmod -R 777 /tmp/Telegram-UserBot
-sudo systemctl start userbot.service
-sudo systemctl enable userbot.service
-echo "
+# Systemd service bringup
+systemd() {
+    sudo mv bot /etc/systemd/system/userbot.service
+    sudo chown -R userbot /tmp/Telegram-UserBot
+    sudo chmod -R 777 /tmp/Telegram-UserBot
+    sudo systemctl start userbot.service
+    sudo systemctl enable userbot.service
+}
+
+# Close down
+close() {
+    echo "
 
 Pushed to init.d. Bot must work on reboot too.
 
 Hope you love using my bot."
-exit
+    exit
+}
+
+#
+# Here's where the mejik happens
+#
+clear
+welcome
+
+createuser
+cd /tmp || exit
+botclone
+
+checkout
+reqinstall
+
+questions
+writeconfig
+
+python3.7 -m userbot test
+systemd
+close
