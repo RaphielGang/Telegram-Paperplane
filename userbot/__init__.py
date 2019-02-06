3270d364171dc167fdbd527ee1c30689229290e1
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-02-07 00:21:08 +0700

    [REFACTOR] : Linting the stuff (2)
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,21 +1,17 @@
 import sqlite3
 import subprocess
-import sys, os
+import sys
+import os
+import logging
+import time
 import dotenv
 from alchemysession import AlchemySessionContainer
-#from argparse import ArgumentParser
-
-#parser = ArgumentParser()
-
-#parser.add_argument(
-##    "-m", help="Run as module?"
-#)
-
-#args = parser.parse_args()
+from sqlalchemy import create_engine
+from telethon import TelegramClient, events
 
 dotenv.load_dotenv("config.env")
-UPDATER=os.environ.get("UPDATER", None)
-BUILD_CHOICE=os.environ.get("BUILD_CHOICE","stable")
+UPDATER = os.environ.get("UPDATER", None)
+BUILD_CHOICE = os.environ.get("BUILD_CHOICE", "stable")
 subprocess.run(["rm", "-rf", "brains.check"], stdout=subprocess.PIPE)
 subprocess.run(
     [
@@ -26,7 +22,7 @@ subprocess.run(
     ],
     stdout=subprocess.PIPE,
 )
-if UPDATER=="True":
+if UPDATER == "True":
     subprocess.run(
         [
             "git",
@@ -55,72 +51,82 @@ if UPDATER=="True":
         ],
         stdout=subprocess.PIPE,
     )
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         if BUILD_CHOICE == "stable":
-            tyq=subprocess.run(
-            [
-            "git",
-            "tag",
-            "-l",
-            ],
-            stdout=subprocess.PIPE,
+            tyq = subprocess.run(
+                [
+                    "git",
+                    "tag",
+                    "-l",
+                ],
+                stdout=subprocess.PIPE,
             ).stdout.decode().split("\n")
+
             subprocess.run(
+                [
+                    "git",
+                    "checkout",
+                    "tags/"+tyq[-2],
+                ],
+                stdout=subprocess.PIPE,
+            )
+
+    if len(sys.argv) == 4:
+        tyq = subprocess.run(
             [
-            "git",
-            "checkout",
-            "tags/"+tyq[-2],
+                "git",
+                "tag",
+                "-l",
             ],
             stdout=subprocess.PIPE,
-            )
-    if len(sys.argv) == 4:
-        tyq=subprocess.run(
-        [
-        "git",
-        "tag",
-        "-l",
-        ],
-        stdout=subprocess.PIPE,
         ).stdout.decode().split("\n")
+
         subprocess.run(
-        [
-        "git",
-        "checkout",
-        "tags/"+tyq[-2],
-        ],
-        stdout=subprocess.PIPE,
+            [
+                "git",
+                "checkout",
+                "tags/"+tyq[-2],
+            ],
+            stdout=subprocess.PIPE,
         )
     print("Your Bot is up-to-date. Bot Spinning up!")
 else:
     print("Updater disabled, spinning the bot without updating.")
-import logging
-from sqlalchemy import create_engine
-from telethon import TelegramClient, events
 
-import time
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 LOGS = logging.getLogger(__name__)
+
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGS.error(
-        "You MUST have a python version of at least 3.6. Multiple features depend on this. Bot quitting."
+        "You MUST have a python version of at least 3.6. \
+        Multiple features depend on this. Bot quitting."
     )
     quit(1)
 try:
     print(___________PLOX_______REMOVE_____THIS_____LINE__________)
 except NameError:
     API_KEY = os.environ.get("API_KEY", None)
+
     API_HASH = os.environ.get("API_HASH", None)
-    LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP","0"))
+
+    LOGGER_GROUP = int(os.environ.get("LOGGER_GROUP", "0"))
+
     LOGGER = os.environ.get(
         "LOGGER", None
     )  # Incase you want to turn off logging, put this to false
+
     PM_AUTO_BAN = os.environ.get("PM_AUTO_BAN", None)
+
     CONSOLE_LOGGER_VERBOSE = os.environ.get("CONSOLE_LOGGER_VERBOSE", None)
+
     DB_URI = os.environ.get("DB_URI", None)
+
     SCREENSHOT_LAYER_ACCESS_KEY = os.environ.get("SCREENSHOT_LAYER_ACCESS_KEY", None)
+
     OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
+
     SUDO = os.environ.get("SUDO", None)
     if CONSOLE_LOGGER_VERBOSE:
         logging.basicConfig(
@@ -130,14 +136,12 @@ except NameError:
         LOGS = logging.getLogger(__name__)
 else:
     LOGS.error(
-        "Your config file seems to be un-edited. Doing so is not allowed. Bot exiting!"
+        "Your config file seems to be un-edited. \
+        Doing so is not allowed. Bot exiting!"
     )
     quit(1)
 bot = TelegramClient("userbot", API_KEY, API_HASH)
-#else:
-#    bot = TelegramClient(None, API_KEY, API_HASH)
-#    bot.session.set_dc(2, "149.154.167.40", 443)
-#    bot.start(phone="9996621234", code_callback=lambda: "22222")
+
 # Global Variables
 SNIPE_TEXT = ""
 COUNT_MSG = 0
@@ -155,6 +159,5 @@ MUTED_USERS = {}
 AFKREASON = "No Reason "
 SPAM_ALLOWANCE = 3
 SPAM_CHAT_ID = []
-BRAIN_CHECKER = []
 DISABLE_RUN = False
-NOTIF_OFF=False
+NOTIF_OFF = False
--- userbot/modules/package.py
+++ userbot/modules/package.py
@@ -3,17 +3,28 @@ import subprocess
 import platform
 from userbot import SUDO
 
-def _start_instalation():
-    package_managers = ["pacman", "apt-get", "yum", "brew", "yaourt", "dnf", "homebrew", "eopkg", "snap"]
-    if "linux" in str(platform.system()).lower():
-            for manager in package_managers:
-                try:
-                    subprocess.check_call(f"which {manager}", shell=True, stderr=subprocess.STDOUT)
-                    install_package(manager)
-                    return
-                except:
-                    continue
 
+def _start_instalation():
+    package_managers = [
+        "pacman",
+        "apt-get",
+        "yum",
+        "brew",
+        "yaourt",
+        "dnf",
+        "homebrew",
+        "eopkg",
+        "snap"]
+
+    if "linux" in str(platform.system()).lower():
+        for manager in package_managers:
+            try:
+                subprocess.check_call(f"which {manager}", shell=True,
+                                      stderr=subprocess.STDOUT)
+                install_package(manager)
+                return
+            except:
+                continue
 
 
 def install_package(package_manager):
@@ -27,18 +38,21 @@ def install_package(package_manager):
         try:
             subprocess.check_call(f"which {package}", shell=True, stderr=subprocess.STDOUT)
         except:
-                to_install.append(package)
+            to_install.append(package)
 
     if to_install:
         if SUDO:
             subprocess.Popen(f"echo {str(SUDO)} | sudo -S {str(package_manager)}" +
-            f" install -y {' '.join(to_install)}", shell=True)
+                             f" install -y {' '.join(to_install)}", shell=True)
         else:
-            subprocess.Popen(f"{str(package_manager)} install -y {' '.join(to_install)}",
-            shell=True)
+            subprocess.Popen(f"{str(package_manager)} install -y \
+                            {' '.join(to_install)}", shell=True)
+
 
 try:
-    if not 'heroku' in os.environ['PATH'] and not os.uname().sysname.lower == 'windows':
+    if not 'heroku' in os.environ['PATH'] and \
+            not os.uname().sysname.lower == 'windows':
         _start_instalation()
+
 except:
     _start_instalation()
