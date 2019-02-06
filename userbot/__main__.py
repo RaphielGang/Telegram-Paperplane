7038e7fbf1813cb0781a82d7dc096578b697051c
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-02-07 01:02:51 +0700

    [REFACTOR] : init: Minor cosmetic stuff (4)
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -19,8 +19,8 @@ for module_name in ALL_MODULES:
     imported_module = importlib.import_module("userbot.modules." + module_name)
 
 LOGS.info("Your Bot is alive! Test it by typing .alive on any chat."
-          "Should you need assistance, head to https: // t.me / userbot_support."
-          "Your Bot Version is 2.1.1-b")
+          "Should you need assistance, head to https://t.me/userbot_support")
+LOGS.info("Your Bot Version is 2.1.1-b")
 
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
