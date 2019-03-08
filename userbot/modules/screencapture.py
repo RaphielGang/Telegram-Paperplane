15e878e2b1436d6ab87e41a6947c5c516660a943
baalajimaestro <baalajimaestro@computer4u.com>
2019-03-08 23:08:27 +0530

    Dual License modules with MPL
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- userbot/modules/screencapture.py
+++ userbot/modules/screencapture.py
@@ -3,6 +3,8 @@
 # Licensed under the Raphielscape Public License, Version 1.b (the "License");
 # you may not use this file except in compliance with the License.
 #
+# The entire source code is OSSRPL except 'screencapture' which is MPL
+# License: MPL and OSSRPL
 
 import os
 import requests
