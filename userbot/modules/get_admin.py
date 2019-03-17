7c64b3c431a6e1a5838c7c8d7f5d0dc0abe70492
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-03-17 20:41:44 +0700

    userbot: treewide: modules: cleanups
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- userbot/modules/get_admin.py
+++ userbot/modules/get_admin.py
@@ -7,8 +7,8 @@
 # License: MPL and OSSRPL
 
 from telethon.errors import ChatAdminRequiredError
-from telethon.tl.types import ChannelParticipantsAdmins, ChatParticipantCreator, Chat
-from userbot import LOGGER, LOGGER_GROUP, HELPER
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot import HELPER
 from userbot.events import register
 
 
@@ -27,6 +27,7 @@ async def get_admin(show):
         except ChatAdminRequiredError as err:
             mentions += " " + str(err) + "\n"
         await show.edit(mentions)
+
 HELPER.update({
-    "adminlist <parmeter>": "Retrieves all admins, sends to LOGGER, if parameter is 0, tags them, if its 1."
+    "adminlist <parmeter>": "Retrieves all admins, if parameter is 0, tags them, if its 1."
 })
