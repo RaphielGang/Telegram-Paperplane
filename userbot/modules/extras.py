5536747b9deb7016af911e96d86266f0fe6dcfb2
Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
2019-03-26 06:56:48 +0000

    Added: .disapprove Haha Yes
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>

--- userbot/modules/extras.py
+++ userbot/modules/extras.py
@@ -8,6 +8,7 @@ from telethon.tl.functions.contacts import BlockRequest
 from telethon.tl.functions.channels import LeaveChannelRequest, CreateChannelRequest, DeleteMessagesRequest
 from lmgtfy import lmgtfy
 from collections import deque
+from telethon.tl.functions.users import GetFullUserRequest
 
 @bot.on(events.NewMessage(outgoing=True, pattern="^.leave$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.leave$"))
@@ -80,3 +81,36 @@ async def source(e):
 async def reedme(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("https://github.com/Yasir-siddiqui/UserBot/blob/master/README.md")
+
+@bot.on(events.NewMessage(outgoing=True, pattern="^.disapprove$"))
+@bot.on(events.MessageEdited(outgoing=True, pattern="^.disapprove$"))
+async def disapprovepm(disapprvpm):
+    if not disapprvpm.text[0].isalpha() and disapprvpm.text[0] not in ("/", "#", "@", "!"):
+        try:
+            from userbot.modules.sql_helper.pm_permit_sql import dissprove
+        except:
+            await disapprvpm.edit("`Running on Non-SQL mode!`")
+            return
+
+        if disapprvpm.reply_to_msg_id:
+            reply = await disapprvpm.get_reply_message()
+            replied_user = await bot(GetFullUserRequest(reply.from_id))
+            aname = replied_user.user.id
+            name0 = str(replied_user.user.first_name)
+            dissprove(replied_user.user.id)
+        else:
+            dissprove(disapprvpm.chat_id)
+            aname = await bot.get_entity(disapprvpm.chat_id)
+            name0 = str(aname.first_name)
+
+        await disapprvpm.edit(
+            f"[{name0}](tg://user?id={disapprvpm.chat_id}) `Disaproved to PM!`"
+            )
+
+        if LOGGER:
+            await bot.send_message(
+                LOGGER_GROUP,
+                f"[{name0}](tg://user?id={disapprvpm.chat_id})"
+                " was disapproved to PM you.",
+            )
+
