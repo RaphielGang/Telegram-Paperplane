605f84899a1b76f0b59a0d32fd53ccc922ccfe0a
Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
2019-03-26 07:05:50 +0000

    Extras: Added .clock animation XD
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>

--- userbot/modules/extras.py
+++ userbot/modules/extras.py
@@ -114,3 +114,12 @@ async def disapprovepm(disapprvpm):
                 " was disapproved to PM you.",
             )
 
+@bot.on(events.NewMessage(pattern=r"\.clock animation", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🕙🕘🕗🕖🕕🕔🕓🕒🕑🕐🕛"))
+	for _ in range(32):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
