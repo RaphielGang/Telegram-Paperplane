6b3875c95efbc51e6bc8b6a3b3c8eb0da2cb50ec
Gegham Zakaryan <zakaryan.2004@outlook.com>
2019-03-29 21:37:14 +0400

    userbot: modules: kang, purge, time: add new features to time and fix bugs
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- userbot/modules/time.py
+++ userbot/modules/time.py
@@ -3,7 +3,6 @@
 # Licensed under the Raphielscape Public License, Version 1.b (the "License");
 # you may not use this file except in compliance with the License.
 #
-# module created by @aragon12
 
 """ Userbot module for getting the date and time of any country or the userbot server.  """
 
@@ -13,27 +12,31 @@ from userbot.events import register
 
 COUNTRY = ''
 
-""" returns the timezone for a given country """
 async def get_tz(con):
+    """ Get time zone of the given country. """
     for c_code in c_n:
         if con == c_n[c_code]:
             return tz(c_tz[c_code][0])
-    return 0
+    try:
+        if c_n[con]:
+            return tz(c_tz[con][0])
+    except KeyError:
+        return
 
-""" For .time command, return the time of
-        1. The country passed as an argument,
-        2. The default userbot country(set it by using .timec),
-        3. The server where the userbot runs.
-"""
 @register(outgoing=True, pattern="^.time ?(.*)")
 async def time_func(tdata):
+    """ For .time command, return the time of
+        1. The country passed as an argument,
+        2. The default userbot country(set it by using .ctime),
+        3. The server where the userbot runs.
+    """
     if not tdata.text[0].isalpha() and tdata.text[0] not in ("/", "#", "@", "!"):
-        con = tdata.pattern_match.group(1).title()
+        con = tdata.pattern_match.group(1)
         t_form = "%l:%M %p"
 
         if not con:
             if not COUNTRY:
-                await tdata.edit(f"`it's` **{dt.now().strftime(t_form)}**  `here.`")
+                await tdata.edit(f"`It's`  **{dt.now().strftime(t_form)}**  `here.`")
                 return
 
             time_zone = await get_tz(COUNTRY)
@@ -44,16 +47,49 @@ async def time_func(tdata):
 
         time_zone = await get_tz(con)
         if not time_zone:
+            await tdata.edit("``` Wrong country given! Try again! ```")
             return
 
-        await tdata.edit("`it's` **"+dt.now(time_zone).strftime(t_form)+"**  `here, in "+con+"`")
+        try:
+            c_name = c_n[con]
+        except KeyError:
+            c_name = con
 
-@register(outgoing=True, pattern="^.date$")
+        await tdata.edit(f"`It's`  **{dt.now(time_zone).strftime(t_form)}**  `in {c_name}`")
+
+@register(outgoing=True, pattern="^.date ?(.*)")
 async def date_func(dat):
+    """ For .date command, return the date of
+        1. The country passed as an argument,
+        2. The default userbot country(set it by using .ctime),
+        3. The server where the userbot runs.
+    """
     if not dat.text[0].isalpha() and dat.text[0] not in ("/", "#", "@", "!"):
-        d_form="%d/%m/%y - %A"
+        d_form = "%d/%m/%y - %A"
+        con = dat.pattern_match.group(1)
 
-        await dat.edit("`it's`  **"+dt.now().strftime(d_form)+"**")
+        if not con:
+            if not COUNTRY:
+                await dat.edit(f"`It's`  **{dt.now().strftime(d_form)}**  `here.`")
+                return
+
+            time_zone = await get_tz(COUNTRY)
+            await dat.edit(
+                f"`It's`  **{dt.now(time_zone).strftime(d_form)}**  `here, in {COUNTRY}.`"
+            )
+            return
+
+        time_zone = await get_tz(con)
+        if not time_zone:
+            await dat.edit("``` Wrong country given! Try again! ```")
+            return
+
+        try:
+            c_name = c_n[con]
+        except KeyError:
+            c_name = con
+
+        await dat.edit(f"`It's`  **{dt.now().strftime(d_form)}**  `in {c_name}`")
 
 @register(outgoing=True, pattern="^.ctime (.*)")
 async def set_time_country(loc):
@@ -62,12 +98,17 @@ async def set_time_country(loc):
         global COUNTRY
         temp_country = loc.pattern_match.group(1)
 
-        time_zone = await get_tz(temp_country.title())
+        time_zone = await get_tz(temp_country)
         if not time_zone:
             await loc.edit("``` Wrong country given! Try again! ```")
             return
+        
+        try:
+            c_name = c_n[temp_country]
+        except KeyError:
+            c_name = temp_country
 
-        COUNTRY = temp_country.title()
+        COUNTRY = c_name
 
         await loc.edit(f"``` Default country for date and time set to {COUNTRY} successfully! ```")
 
