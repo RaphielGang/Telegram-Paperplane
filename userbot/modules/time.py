47d7e557c60805721c40b83bff3ad6c62fce4f73
zakaryan2004 <zakaryan.2004@outlook.com>
2019-03-29 16:14:35 +0530

    modules: add ability to change default timezone
    most of the guys uses heroku or other servers to host bot.. its difficult to change system timezone there
    
    add a feature to change default timezone..
    when changed.. the bot will use that as default timezone.. instead of server timezone
    
    usage:
     .ctime 'country' to change default timezone
     .time to check

--- userbot/modules/time.py
+++ userbot/modules/time.py
@@ -9,21 +9,30 @@ from datetime import datetime as dt
 from pytz import country_names as c_n, country_timezones as c_tz, timezone as tz
 from userbot.events import register
 
+COUNTRY = ''
+
+""" returns the timezone for a given country """
 def get_tz(con):
     for c_code in c_n:
         if con == c_n[c_code]:
             return tz(c_tz[c_code][0])
     return 0
 
-@register(outgoing=True, pattern="^.time")
+@register(outgoing=True, pattern="^.time ?(.*)")
 async def time_func(tdata):
     if not tdata.text[0].isalpha() and tdata.text[0] not in ("/", "#", "@", "!"):
-        msg = str(tdata.text)
-        con = str(msg[6:]).title()
+        con = tdata.pattern_match.group(1).title()
         t_form = "%l:%M %p"
 
         if not con:
-            await tdata.edit("`it's` **"+dt.now().strftime(t_form)+"**")
+            if not COUNTRY:
+                await tdata.edit(f"`it's` **{dt.now().strftime(t_form)}**  `here.`")
+                return
+
+            time_zone = get_tz(COUNTRY)
+            await tdata.edit(
+                f"`It's`  **{dt.now(time_zone).strftime(t_form)}**  `here, in {COUNTRY}`"
+            )
             return
 
         time_zone = get_tz(con)
@@ -38,3 +47,19 @@ async def date_func(dat):
         d_form="%d/%m/%y - %A"
 
         await dat.edit("`it's`  **"+dt.now().strftime(d_form)+"**")
+
+@register(outgoing=True, pattern="^.ctime (.*)")
+async def set_time_country(loc):
+    """ For .ctime command, change the default userbot country for date and time commands. """
+    if not loc.text[0].isalpha() and loc.text[0] not in ("/", "#", "@", "!"):
+        global COUNTRY
+        temp_country = loc.pattern_match.group(1)
+
+        time_zone = get_tz(temp_country.title())
+        if not time_zone:
+            await loc.edit("``` Wrong country given! Try again! ```")
+            return
+
+        COUNTRY = temp_country.title()
+
+        await loc.edit(f"``` Default country for date and time set to {COUNTRY} successfully! ```")
