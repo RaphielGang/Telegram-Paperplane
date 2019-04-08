4ed1649d374cb583471b3bbd3f29fd9901ddd0bf
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-04-08 12:53:33 +0700

    treewide: cleanups
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- userbot/modules/weather.py
+++ userbot/modules/weather.py
@@ -42,7 +42,7 @@ async def get_weather(weather):
     if len(OWM_API) < 1:
         await weather.edit("Get an API key from https://openweathermap.org/ first.")
         return
-    
+
     APPID = OWM_API
 
     if not weather.pattern_match.group(1):
@@ -53,9 +53,9 @@ async def get_weather(weather):
     else:
         CITY = weather.pattern_match.group(1)
 
-    timezone_countries = {timezone: country 
-                      for country, timezones in c_tz.items()
-                      for timezone in timezones}
+    timezone_countries = {timezone: country
+                          for country, timezones in c_tz.items()
+                          for timezone in timezones}
 
     if "," in CITY:
         newcity = CITY.split(",")
@@ -95,14 +95,14 @@ async def get_weather(weather):
     fullc_n = c_n[f"{country}"]
 
     fahrenheit = str(((curtemp - 273.15) * 9/5 + 32)).split(".")
-    celsius =  str((curtemp - 273.15)).split(".")
+    celsius = str((curtemp - 273.15)).split(".")
 
     await weather.edit(f"**Temperature:** {celsius[0]}°C / {fahrenheit[0]}°F\n"
-        +f"**Humidity:** {humidity}%\n"
-        +f"**Wind:** {wind} m/s\n\n\n"
-        +f"**{desc}**\n"
-        +f"`{cityname}, {fullc_n}`\n"
-        +f"`{time}`")
+                       +f"**Humidity:** {humidity}%\n"
+                       +f"**Wind:** {wind} m/s\n\n\n"
+                       +f"**{desc}**\n"
+                       +f"`{cityname}, {fullc_n}`\n"
+                       +f"`{time}`")
 
 @register(outgoing=True, pattern="^.setcity ?(.*)")
 async def set_default_city(city):
@@ -113,7 +113,7 @@ async def set_default_city(city):
     if len(OWM_API) < 1:
         await city.edit("Get an API key from https://openweathermap.org/ first.")
         return
-    
+
     global DEFCITY
     APPID = OWM_API
 
@@ -125,9 +125,9 @@ async def set_default_city(city):
     else:
         CITY = city.pattern_match.group(1)
 
-    timezone_countries = {timezone: country 
-                      for country, timezones in c_tz.items()
-                      for timezone in timezones}
+    timezone_countries = {timezone: country
+                          for country, timezones in c_tz.items()
+                          for timezone in timezones}
 
     if "," in CITY:
         newcity = CITY.split(",")
@@ -140,7 +140,7 @@ async def set_default_city(city):
             except KeyError:
                 await city.edit("Invalid country.")
                 return
-            CITY = newcity[0].strip() + "," + countrycode.strip()        
+            CITY = newcity[0].strip() + "," + countrycode.strip()
 
     url = f'https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={APPID}'
     request = requests.get(url)
