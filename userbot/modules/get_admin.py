21cb7e86ab164d8235a0d79637f72542a6440a78
aragon12 <sm95007@gmail.com>
2019-04-11 08:19:39 +0530

    module: add delusers
    delusers is a module.. which will help you keep your groups clean
    it searches for deleted accounts.. and also removes them
    
    usage:
      .delusers [searches for deleted acc]
      .delusers clean [searches and removes deleted acc]

--- userbot/modules/get_admin.py
+++ userbot/modules/get_admin.py
@@ -8,10 +8,34 @@
 
 """ Userbot module allowing you to get the admin list in a chat. """
 
-from telethon.errors import ChatAdminRequiredError
-from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
+from telethon.tl.types import ChannelParticipantsAdmins, ChatBannedRights
+from telethon.tl.functions.channels import EditBannedRequest
 from userbot import HELPER
 from userbot.events import register
+
+banned_rights = ChatBannedRights(
+            until_date=None,
+            view_messages=True,
+            send_messages=True,
+            send_media=True,
+            send_stickers=True,
+            send_gifs=True,
+            send_games=True,
+            send_inline=True,
+            embed_links=True,
+        )
+
+unbanned_rights = ChatBannedRights(
+            until_date=None,
+            send_messages=None,
+            send_media=None,
+            send_stickers=None,
+            send_gifs=None,
+            send_games=None,
+            send_inline=None,
+            embed_links=None,
+            )
 
 
 @register(outgoing=True, pattern="^.adminlist$")
@@ -38,7 +62,87 @@ async def get_admin(show):
             mentions += " " + str(err) + "\n"
         await show.edit(mentions, parse_mode="html")
 
+
+@register(outgoing=True, pattern="^.delusers ?(.*)")
+async def get_admin(show):
+    """ For .adminlist command, list all of the admins of the chat. """
+    if not show.text[0].isalpha() and show.text[0] not in ("/", "#", "@", "!"):
+        con = show.pattern_match.group(1)
+        del_u = 0
+        del_status = "`No deleted accounts found, Group is cleaned as Hell`"
+
+        if not show.is_group:
+            await show.edit("`This command is only for groups!`")
+            return
+
+        if con != "clean":
+            await show.edit("`Searching for zombie accounts...`")
+            async for user in show.client.iter_participants(
+                    show.chat_id
+            ):
+                if user.deleted:
+                    del_u += 1
+
+            if del_u > 0:
+                del_status = f"`found`  **{del_u}**  `deleted account(s) in this group\nclean them by using .delusers clean`"
+            await show.edit(del_status)
+            return
+
+        # Here laying the sanity check
+        chat = await show.get_chat()
+        admin = chat.admin_rights
+        creator = chat.creator
+
+        # Well
+        if not admin and not creator:
+            await show.edit("`You aren't an admin here!`")
+            return
+
+        await show.edit("`Cleaning deleted accounts...`")
+        del_u = 0
+        del_a = 0
+
+        async for user in show.client.iter_participants(
+                    show.chat_id
+            ):
+            if user.deleted:
+                try:
+                    await show.client(
+                        EditBannedRequest(
+                            show.chat_id,
+                            user.id,
+                            banned_rights
+                        )
+                    )
+                except UserAdminInvalidError:
+                    del_u -= 1
+                    del_a += 1
+                await show.client(
+                        EditBannedRequest(
+                            show.chat_id,
+                            user.id,
+                            unbanned_rights
+                        )
+                    )
+                del_u += 1
+
+        if del_u > 0:
+            del_status = f"`cleaned`  **{del_u}**  `deleted account(s)`"
+
+        if del_a > 0:
+            del_status = f"`cleaned`  **{del_u}**  `deleted account(s)`\n**{del_a}**  `deleted admin accounts are not removed`"
+
+        await show.edit(del_status)
+
+
 HELPER.update({
     "adminlist": ".adminlist\
     \nUsage: Retrieves all admins in the chat."
+})
+
+HELPER.update({
+    'delusers': '.delusers\
+\nUsage: Searches for deleted accounts in a group.\
+\n\n.delusers clean\
+\nUsage: Searches and removes deleted accounts from the group'
 })
