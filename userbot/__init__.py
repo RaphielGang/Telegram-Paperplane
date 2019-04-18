012a7ad1698b8094410a55c7d89b503003b9a6c0
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-04-18 13:56:09 +0700

    init: disable invalid-name pylint message
    We're basically keeping this as-is until I got solution for the
    event-based bump of this part
    
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -83,8 +83,8 @@ SPOTIFY_PASS = os.environ.get("SPOTIFY_PASS", None)
 SPOTIFY_BIO_PREFIX = os.environ.get("SPOTIFY_BIO_PREFIX", None)
 DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
 
+# pylint: disable=invalid-name
 bot = TelegramClient("userbot", API_KEY, API_HASH)
-
 
 if os.path.exists("learning-data-root.check"):
     os.remove("learning-data-root.check")
