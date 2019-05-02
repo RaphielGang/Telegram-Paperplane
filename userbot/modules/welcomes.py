933e3822f4c6d6877da507e5223d5999c148cceb
Geg Zakaryan <zakaryan.2004@outlook.com>
2019-05-02 18:20:43 +0400

    modules: welcome_mute: cleanups, extend the algorith, fix bugs (#95)
    * modules: welcome_mute: cleanups, extend the algorith, fix bugs
    
    * Cleaned up the code(from pylint -8.5 score to 9.56)
    * Fixed up some bugs such as using loop variable outside the loop
    * Remove unnecessary imports
    * Extend the algorithm by adding Announcements Bot name to the list
    * Add helper to the module
    * Fix the logger logic(the module now checks if logger is enabled or not)
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    
    * modules: welcome_mute: fix lots of logic issues
    * Fix a bug when the userbot wouldn't detect the spambot if it was added by someone else
    * Fixed logger not logging in all cases
    * Show the ID of the banned user in the message(more info should be added here)
    * Much more...
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    
    * modules: welcome_mute: delete debugging code, add permanent link to the user instead
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    
    * modules: welcome_mute: fix the old message detecting bug, more
    
    * Fixed the bug which made the userbot to detect messages which were sent before the user joined the group.
    * Fixed the permanent user link in the message.
    * Added a new spambot check to the algorithm.
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- userbot/modules/welcomes.py
+++ userbot/modules/welcomes.py
@@ -4,74 +4,126 @@
 # you may not use this file except in compliance with the License.
 #
 
-from userbot import LOGGER, LOGGER_GROUP, HELPER, WELCOME_MUTE, bot
-from userbot.events import register
-import asyncio
-from userbot.modules.admin import BANNED_RIGHTS, UNBAN_RIGHTS
-from telethon.errors import (BadRequestError, ChatAdminRequiredError,
-                             ImageProcessFailedError, PhotoCropSizeSmallError,
-                             UserAdminInvalidError)
+''' A module for helping ban group join spammers. '''
 
-from telethon.tl.functions.channels import (EditAdminRequest,
-                                            EditBannedRequest,
-                                            EditPhotoRequest)
-from telethon.tl.types import (ChannelParticipantsAdmins, ChatAdminRights,
-                               ChatBannedRights, MessageEntityMentionName,
-                               MessageMediaPhoto)
+from asyncio import sleep
+
+from telethon.tl.functions.channels import EditBannedRequest
 from telethon import events
-Spambot=False
+
+from userbot import LOGGER, LOGGER_GROUP, HELPER, WELCOME_MUTE, bot
+from userbot.modules.admin import BANNED_RIGHTS, UNBAN_RIGHTS
+
+
 @bot.on(events.ChatAction)
 async def welcome_mute(welcm):
-  if welcm.user_joined or welcm.user_added:
-    from asyncio import sleep
-    await sleep(5)
-    if not WELCOME_MUTE:
-        return
-    async for message in bot.iter_messages(welcm.chat_id, from_user=welcm.action_message.from_id):
-        global Spambot
-        user = await welcm.client.get_entity(welcm.action_message.from_id)
-        print(message.text)
-        if "http://" in message.text:
-            Spambot=True
-        elif "t.me" in message.text:
-            Spambot=True
-        elif message.fwd_from:
-            Spambot=True
-        elif "https://" in message.text:
-            Spambot=True
-        else:
-            if user.first_name in ("Bitmex", "Promotion", "Information", "Dex"):
-                if user.last_name == "Bot":
-                    Spambot=True
-        break
-    if Spambot:
-        await welcm.reply("`Potential SpamBot Detected! Kicking away! Will log the ID for further purposes!`")
-        chat = await welcm.get_chat()
-        admin = chat.admin_rights
-        creator = chat.creator
-        if not admin and not creator:
-            await welcm.reply("@admins \n`ANTI SPAMBOT DETECTOR! \nTHIS USER MATCHES MY ALGORITHMS AS A SPAMBOT!`")
+    ''' Ban a recently joined user if it matches the spammer checking algorithm. '''
+    if welcm.user_joined or welcm.user_added:
+        users = []
+
+        if welcm.user_added:
+            for i in welcm.action_message.action.users:
+                users.append(i)
+
+        spambot = False
+
+        await sleep(5)
+
+        if not WELCOME_MUTE:
             return
-        try:
-            await welcm.client(
-                EditBannedRequest(
+
+        for user_id in users:
+            async for message in bot.iter_messages(
                     welcm.chat_id,
-                    welcm.action_message.from_id,
-                    BANNED_RIGHTS
-                   )
-             )
-            await welcm.client(EditBannedRequest(
-                welcm.chat_id,
-                welcm.action_message.from_id,
-                UNBAN_RIGHTS
-                ))
-        except:
-            await welcm.reply("@admins \n`ANTI SPAMBOT DETECTOR! \nTHIS USER MATCHES MY ALGORITHMS AS A SPAMBOT!`")
-            return
-        await message.delete()
-        await welcm.client.send_message(
+                    from_user=user_id
+            ):
+                if not message: break
+                
+                join_time = welcm.action_message.date
+                message_date = message.date
+
+                if message_date < join_time:
+                    continue # The message was sent before the user joined, thus ignore it
+
+                ### DEBUGGING. LEAVING IT HERE FOR SOME TIME ###
+                print(f"User Joined: {join_time}")
+                print(f"Spam Message Sent: {message_date}")
+                ###
+                
+                user = await welcm.client.get_entity(user_id)
+                if "http://" in message.text:
+                    spambot = True
+                elif "t.me" in message.text:
+                    spambot = True
+                elif message.fwd_from:
+                    spambot = True
+                elif "https://" in message.text:
+                    spambot = True
+                else:
+                    if user.first_name in (
+                            "Bitmex",
+                            "Promotion",
+                            "Information",
+                            "Dex",
+                            "Announcements", 
+                            "Info"
+                    ):
+                        if user.last_name == "Bot":
+                            spambot = True
+
+                if spambot:
+                    print(f"Potential Spam Message: {message.text}")
+                    await message.delete()
+                    break
+
+                continue # Check the next messsage
+
+        if spambot:
+            await welcm.reply(
+                "`Potential Spambot Detected! Kicking away! "
+                "Will log the ID for further purposes!\n"
+                f"USER:` [{user.first_name}](tg://user?id={user.id})")
+
+            chat = await welcm.get_chat()
+            admin = chat.admin_rights
+            creator = chat.creator
+            if not admin and not creator:
+                await welcm.reply(
+                    "@admins\n"
+                    "`ANTI SPAMBOT DETECTOR!\n"
+                    "THIS USER MATCHES MY ALGORITHMS AS A SPAMBOT!`")
+            else:
+                try:
+                    await welcm.client(
+                        EditBannedRequest(
+                            welcm.chat_id,
+                            user.id,
+                            BANNED_RIGHTS
+                        )
+                    )
+                    await welcm.client(
+                        EditBannedRequest(
+                            welcm.chat_id,
+                            user.id,
+                            UNBAN_RIGHTS
+                        )
+                    )
+                except:
+                    await welcm.reply(
+                        "@admins\n"
+                        "`ANTI SPAMBOT DETECTOR!\n"
+                        "THIS USER MATCHES MY ALGORITHMS AS A SPAMBOT!`")
+
+            if LOGGER:
+                await welcm.client.send_message(
                     LOGGER_GROUP,
                     "#SPAMBOT-KICK\n"
-                    f"USER: [{user.first_name}](tg://user?id={welcm.action_message.from_id})\n"
+                    f"USER: [{user.first_name}](tg://user?id={user.id})\n"
                     f"CHAT: {welcm.chat.title}(`{welcm.chat_id}`)"
                 )
+
+HELPER.update({
+    'welcome_mute': "If enabled in config.env or env var, \
+        this module will ban(or inform admins) the group join \
+        spammers if they match the userbot's algorithm of banning"
+})
