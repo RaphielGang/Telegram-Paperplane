01c5e84d4612948c8a0727d2d70e1abf1ca2fcd7
Ali Hasan <ahb7671@gmail.com>
2019-05-14 11:36:02 +0000

    [Tspam] New Module.
    Ali hasan is pro Haha yash

--- userbot/modules/spammer.py
+++ userbot/modules/spammer.py
@@ -10,6 +10,13 @@ from asyncio import wait
 from userbot import LOGGER_GROUP, LOGGER, HELPER
 from userbot.events import register
 
+@register(outgoing=True, pattern="^.tspam")
+async def tmeme(e):
+    tspam = str(e.text[7:])
+    message = tspam.replace(" ", "")
+    for letter in message:
+        await e.respond(letter)
+    await e.delete()
 
 @register(outgoing=True, pattern="^.spam")
 async def spammer(e):
