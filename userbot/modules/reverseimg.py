12f52d67edda831f009e8c9cf1ad30ea70df8610
noobvishal <48903549+noobvishal@users.noreply.github.com>
2019-05-20 17:24:51 +0530

    Update reverseimg.py

--- userbot/modules/reverseimg.py
+++ userbot/modules/reverseimg.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-""" Userbot module for kanging stickers or making new ones. """
+""" Userbot module for reverse image search from google. """
 
 import io
 import os
