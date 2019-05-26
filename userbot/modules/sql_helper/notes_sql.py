niteshraj2310/RemixGeng

Fixed get notes
Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

userbot/modules/sql_helper/notes_sql.py
@@ -16,7 +16,7 @@ class Notes(BASE):
 Notes.__table__.create(checkfirst=True)
 def get_notes(chat_id):
     try:
-        return SESSION.query(Notes).all()
+        return SESSION.query(Notes).filter(Filters.chat_id==str(chat_id)).all()
     finally:
         SESSION.close()
 def add_note(chat_id, keyword, reply):

RaphielGang/Telegram-Paperplane

userbot/modules/sql_helper/notes_sql.py
@@ -16,7 +16,7 @@ class Notes(BASE):
 Notes.__table__.create(checkfirst=True)
 def get_notes(chat_id):
     try:
-        return SESSION.query(Notes).all()
+        return SESSION.query(Notes).filter(Notes.chat_id==str(chat_id)).all()
     finally:
         SESSION.close()
 def add_note(chat_id, keyword, reply):