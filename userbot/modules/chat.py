1925c589a244eb086aaed944ffdf7f5c358b8d12
Kandarp <Kandnub@gmail.com>
2019-06-08 12:10:34 +0530

    Fix Few Things in to-do
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -44,15 +44,21 @@ async def chatidgetter(chat):
         await chat.edit("Chat ID: `" + str(chat.chat_id) + "`")
 
 
-@register(outgoing=True, pattern="^.log")
+@register(outgoing=True, pattern=r"^.log(?: |$)([\s\S]*)")
 async def log(log_text):
     """ For .log command, forwards a message or the command argument to the bot logs group """
     if not log_text.text[0].isalpha() and log_text.text[0] not in ("/", "#", "@", "!"):
-        textx = await log_text.get_reply_message()
-        message = textx
-        message = str(message.message)
         if BOTLOG:
-            await (await log_text.get_reply_message()).forward_to(BOTLOG_CHATID)
+            if log_text.reply_to_msg_id:
+                reply_msg = await log_text.get_reply_message()
+                await reply_msg.forward_to(BOTLOG_CHATID)
+            elif log_text.pattern_match.group(1):
+                user = f"#LOG / Chat ID: {log_text.chat_id}\n\n"
+                textx = user + log_text.pattern_match.group(1)
+                await bot.send_message(BOTLOG_CHATID, textx)
+            else:
+                await log_text.edit("`What am I supposed to log?`")
+                return
             await log_text.edit("`Logged Successfully`")
         else:
             await log_text.edit("`This feature requires Logging to be enabled!`")
