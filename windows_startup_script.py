06c8170a80527ab1cca66409295874e276c9a4bb
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-06-11 18:44:30 +0700

    session_gen: refactor
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

--- windows_startup_script.py
+++
@@ -1,8 +0,0 @@
-from telethon import TelegramClient, events
-API_KEY="Type that here"
-API_HASH="Type that here"
-#get it from my.telegram.org
-bot = TelegramClient('userbot',API_KEY,API_HASH)
-bot.start()
-
-#This script wont run your bot, it just generates a session.
