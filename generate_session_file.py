0af7dc099ee646ad8815d61f775e9197d65fdd33
Raphiel Rollerscaperers <raphielscape@outlook.com>
2019-06-11 18:44:30 +0700

    session_gen: refactor
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>

---
+++ generate_session_file.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.b (the "License");
+# you may not use this file except in compliance with the License.
+#
+# This script wont run your bot, it just generates a session.
+
+from telethon import TelegramClient, events
+
+load_dotenv("config.env")
+
+API_KEY = os.environ.get("API_KEY", None)
+API_HASH = os.environ.get("API_HASH", None)
+
+bot = TelegramClient('userbot', API_KEY, API_HASH)
+bot.start()
