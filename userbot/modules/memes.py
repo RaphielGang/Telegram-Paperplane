ad3167b699d4cd39533a61da41cd5d19f723a847
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-06-18 16:52:55 +0530

    added slaps !!
    and other little memes !!

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -12,6 +12,11 @@ import random
 import re
 import time
 
+from collections import deque
+
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+
 from spongemock import spongemock
 from zalgo_text import zalgo
 
@@ -191,7 +196,118 @@ RUNSREACTS = [
     "I go away",
     "I am just walking off, coz me is too fat.",
     "I Fugged off!",
+    "Will run for chocolate.",
+    "I run because I really like food.",
+    "Running...because dieting is not an option.",
+    "Wicked fast runnah",
+    "If you wanna catch me, you got to be fast...if you wanna stay with me, you got to be good...if you wanna pass me...You've got to be kidding.",
+    "Anyone can run a hundred meters, it's the next forty-two thousand and two hundred that count.",
+    "Why are all these people following me?",
+    "Are the kids still chasing me?",
+    "Running a marathon...there's an app for that.",
 ]
+HELLOSTR = [
+    "Hi !",
+    "‘Ello, gov'nor!",
+    "What’s crackin’?",
+    "‘Sup, homeslice?",
+    "Howdy, howdy ,howdy!",
+    "Hello, who's there, I'm talking.",
+    "You know who this is.",
+    "Yo!",
+    "Whaddup.",
+    "Greetings and salutations!",
+    "Hello, sunshine!",
+    "Hey, howdy, hi!",
+    "What’s kickin’, little chicken?",
+    "Peek-a-boo!",
+    "Howdy-doody!",
+    "Hey there, freshman!",
+    "I come in peace!",
+    "Ahoy, matey!",
+    "Hiya!",
+]
+
+SHGS = [
+    r"¯\_(ツ)_/¯",
+    r"¯\(°_o)/¯",
+    r"t(ツ)_/¯",
+    r"¯\_ಠ_ಠ_/¯",
+    r"¯\_(⌣̯̀⌣́)_/¯",
+    r"˘\_( õ ‹3 ó)_/˘",
+    r"¯\(◉‿◉)/¯",
+    r"¯\_(☯෴☯)_/¯",
+    r"乁[ᓀ˵▾˵ᓂ]ㄏ",
+    r"¯\(©¿©) /¯",
+    r"へ‿(ツ)‿ㄏ",
+    r"¯\_▒ – ﹏ – ▒_/¯",
+    r"¯\_| ✖ 〜 ✖ |_/¯",
+    r"¯\_ȌᴥȌ_/¯",
+    r"¯\_╏ ՞ ︿ ՞ ╏_/¯",
+    r"¯\_༼ᴼل͜ᴼ༽_/¯",
+]
+
+SLAP_TEMPLATES = [
+    "{hits} {user2} with a {item}.",
+    "{hits} {user2} in the face with a {item}.",
+    "{hits} {user2} around a bit with a {item}.",
+    "{throws} a {item} at {user2}.",
+    "grabs a {item} and {throws} it at {user2}'s face.",
+    "launches a {item} in {user2}'s general direction.",
+    "starts slapping {user2} silly with a {item}.",
+    "pins {user2} down and repeatedly {hits} them with a {item}.",
+    "grabs up a {item} and {hits} {user2} with it.",
+    "ties {user2} to a chair and {throws} a {item} at them.",
+    "gave a friendly push to help {user2} learn to swim in lava."
+]
+
+ITEMS = [
+    "cast iron skillet",
+    "large trout",
+    "baseball bat",
+    "cricket bat",
+    "wooden cane",
+    "nail",
+    "printer",
+    "shovel",
+    "CRT monitor",
+    "physics textbook",
+    "toaster",
+    "portrait of Richard Stallman",
+    "television",
+    "five ton truck",
+    "roll of duct tape",
+    "book",
+    "laptop",
+    "old television",
+    "sack of rocks",
+    "rainbow trout",
+    "rubber chicken",
+    "spiked bat",
+    "fire extinguisher",
+    "heavy rock",
+    "chunk of dirt",
+    "beehive",
+    "piece of rotten meat",
+    "bear",
+    "ton of bricks",
+]
+
+THROW = [
+    "throws",
+    "flings",
+    "chucks",
+    "hurls",
+]
+
+HIT = [
+    "hits",
+    "whacks",
+    "slaps",
+    "smacks",
+    "bashes",
+]
+
 DISABLE_RUN = False
 # ===========================================
 
@@ -221,6 +337,76 @@ async def kek(keks):
         time.sleep(0.3)
         await keks.edit(":" + uio[i % 2])
 
+@register(pattern=".slap(?: |$)(.*)", outgoing=True)
+async def who(event):
+    """ slaps a user, or get slapped if not a reply. """
+    if event.fwd_from:
+        return
+
+    replied_user = await get_user(event)
+    caption = await slap(replied_user, event)
+    message_id_to_reply = event.message.reply_to_msg_id
+
+    if not message_id_to_reply:
+        message_id_to_reply = None
+
+    try:
+        await event.edit(caption)
+
+    except:
+        await event.edit("`Can't slap this nibba !!`")
+
+async def get_user(event):
+    """ Get the user from argument or replied message. """
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        replied_user = await event.client(GetFullUserRequest(previous_message.from_id))
+    else:
+        user = event.pattern_match.group(1)
+
+        if user.isnumeric():
+            user = int(user)
+
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+
+        if event.message.entities is not None:
+            probable_user_mention_entity = event.message.entities[0]
+
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user
+        try:
+            user_object = await event.client.get_entity(user)
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
+
+        except (TypeError, ValueError):
+            await event.edit("`I don't slap strangers !!`")
+            return None
+
+    return replied_user
+
+async def slap(replied_user, event):
+    """ Construct a funny slap sentence !! """
+    user_id = replied_user.user.id
+    first_name = replied_user.user.first_name
+    username = replied_user.user.username
+
+    if username:
+        slapped = "@{}".format(username)
+    else:
+        slapped = f"[{first_name}](tg://user?id={user_id})"
+
+    temp = random.choice(SLAP_TEMPLATES)
+    item = random.choice(ITEMS)
+    hit = random.choice(HIT)
+    throw = random.choice(THROW)
+
+    caption = "..." + temp.format(user2=slapped, item=item, hits=hit, throws=throw)
+
+    return caption
 
 @register(outgoing=True, pattern="^-_-$")
 async def lol(lel):
@@ -230,6 +416,17 @@ async def lol(lel):
         okay = okay[:-1] + "_-"
         await lel.edit(okay)
 
+@register(outgoing=True, pattern="^;__;$")
+async def fun(e):
+    t = ";__;"
+    for j in range(10):
+        t = t[:-1] + "_;"
+        await e.edit(t)
+
+@register(outgoing=True, pattern="^.cry$")
+async def cry(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("(;´༎ຶД༎ຶ)")
 
 @register(outgoing=True, pattern="^.cp(?: |$)(.*)")
 async def copypasta(cp_e):
@@ -330,10 +527,12 @@ async def zal(zgfy):
         await zgfy.edit(zalgofied_text)
 
 
-@register(outgoing=True, pattern="^hi$")
+@register(outgoing=True, pattern="^.hi$")
 async def hoi(hello):
     """ Greet everyone! """
-    await hello.edit("Hoi!😄")
+    index = random.randint(0, len(HELLOSTR))
+    reply_text = HELLOSTR[index]
+    await hello.edit(reply_text)
 
 
 @register(outgoing=True, pattern="^.owo(?: |$)(.*)")
@@ -373,7 +572,9 @@ async def react_meme(react):
 async def shrugger(shg):
     r""" ¯\_(ツ)_/¯ """
     if not shg.text[0].isalpha() and shg.text[0] not in ("/", "#", "@", "!"):
-        await shg.edit(r"¯\_(ツ)_/¯")
+        index = random.randint(0, len(SHGS))
+        reply_text = SHGS[index]
+        await shg.edit(reply_text)
 
 
 @register(outgoing=True, pattern="^.runs$")
@@ -392,7 +593,7 @@ async def disable_runs(norun):
     if not norun.text[0].isalpha() and norun.text[0] not in ("/", "#", "@", "!"):
         global DISABLE_RUN
         DISABLE_RUN = True
-        await norun.edit("```Done!```")
+        await norun.edit("```Disabled .runs !!```")
 
 
 @register(outgoing=True, pattern="^.enable runs$")
@@ -401,7 +602,7 @@ async def enable_runs(run):
     if not run.text[0].isalpha() and run.text[0] not in ("/", "#", "@", "!"):
         global DISABLE_RUN
         DISABLE_RUN = False
-        await run.edit("```Done!```")
+        await run.edit("```Enabled .runs !!```")
 
 
 @register(outgoing=True, pattern="^.metoo$")
@@ -412,6 +613,37 @@ async def metoo(hahayes):
         reply_text = METOOSTR[index]
         await hahayes.edit(reply_text)
 
+@register(outgoing=True, pattern="^Oof$")
+async def Oof(e):
+    t = "Oof"
+    for j in range(15):
+        t = t[:-1] + "of"
+        await e.edit(t)
+
+@register(outgoing=True, pattern="^.10iq$")
+async def iqless(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("♿")
+
+@register(outgoing=True, pattern="^.moon$")
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
+	for _ in range(32):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+
+@register(outgoing=True, pattern="^.clock$")
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🕙🕘🕗🕖🕕🕔🕓🕒🕑🕐🕛"))
+	for _ in range(32):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
 
 @register(outgoing=True, pattern="^.mock(?: |$)(.*)")
 async def spongemocktext(mock):
@@ -488,5 +720,53 @@ async def typewriter(typew):
             await asyncio.sleep(sleep_time)
 
 CMD_HELP.update({
-    "memes": "Ask Thoncc (@Skittles9823Bot) for that."
+    "memes": ".cowsay\
+\nUsage: cow which says things.\
+\n\n:/\
+\nUsage: Check yourself ;)\
+\n\n-_-\
+\nUsage: Ok...\
+\n\n;__;\
+\nUsage: Like `-_-` but crying.\
+\n\n.cp\
+\nUsage: Copypasta the famous meme\
+\n\n.vapor\
+\nUsage: Vaporize everything!\
+\n\n.str\
+\nUsage: Stretch it.\
+\n\n.10iq\
+\nUsage: You retard !!\
+\n\n.zal\
+\nUsage: Invoke the feeling of chaos.\
+\n\nOof\
+\nUsage: Ooooof\
+\n\n.moon\
+\nUsage: kensar moon animation.\
+\n\n.clock\
+\nUsage: kensar clock animation.\
+\n\n.hi\
+\nUsage: Greet everyone!\
+\n\n.owo\
+\nUsage: UwU\
+\n\n.react\
+\nUsage: Make your userbot react to everything.\
+\n\n.slap\
+\nUsage: reply to slap them with random objects !!\
+\n\n.cry\
+\nUsage: y u du dis, i cri.\
+\n\n.shg\
+\nUsage: Shrug at it !!\
+\n\n.runs\
+\nUsage: Run, run, RUNNN! [`.disable runs`: disable | `.enable runs`: enable]\
+\n\n.metoo\
+\nUsage: Haha yes\
+\n\n.mock\
+\nUsage: Do it and find the real fun.\
+\n\n.clap\
+\nUsage: Praise people!\
+\n\n.bt\
+\nUsage: Believe me, you will find this useful.\
+\n\n.type\
+\nUsage: ust a small command to make your keyboard become a typewriter!\
+\n\n\nThanks to Thoncc (@Skittles9823Bot) for these."
 })
