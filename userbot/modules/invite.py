4eb123ffe5503d1850c371bc81f1ef520a7efb6c
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-30 23:48:42 +0530

    Added Invite Module
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

---
+++ userbot/modules/invite.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot module for inviting users to group """
+
+from telethon import functions
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.invite(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    to_add_users = event.pattern_match.group(1)
+    if event.is_private:
+        await event.edit("`.invite` users to a chat, not to a Private Message")
+    else:
+        if not event.is_channel and event.is_group:
+            # https://lonamiwebs.github.io/Telethon/methods/messages/add_chat_user.html
+            for user_id in to_add_users.split(" "):
+                try:
+                    await event.client(functions.messages.AddChatUserRequest(
+                        chat_id=event.chat_id,
+                        user_id=user_id,
+                        fwd_limit=1000000
+                    ))
+                except Exception as e:
+                    await event.reply(str(e))
+            await event.edit("Invited Successfully")
+        else:
+            # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
+            for user_id in to_add_users.split(" "):
+                try:
+                    await event.client(functions.channels.InviteToChannelRequest(
+                        channel=event.chat_id,
+                        users=[user_id]
+                    ))
+                except Exception as e:
+                    await event.reply(str(e))
+            await event.edit("Invited Successfully")
