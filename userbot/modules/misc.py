c119bf6678d675d3be0879a6ae638f5d75956521
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2019-09-24 23:23:47 +0700

    modules / misc : Reflect new URL
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -79,7 +79,7 @@ async def bot_support(wannahelp):
 @register(outgoing=True, pattern="^.repo$")
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
-    await wannasee.edit("https://github.com/RaphielGang/Telegram-UserBot/")
+    await wannasee.edit("https://github.com/RaphielGang/Telegram-Paperplane")
 
 
 CMD_HELP.update({
