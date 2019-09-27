662e4a20c0af758c0e825f562b526bd85c3179fd
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2019-09-27 18:01:51 +0700

    paperplane / events : Refactor
    In preparation for the rewrite, trim the statements down.
    
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- userbot/events.py
+++ userbot/events.py
@@ -21,43 +21,45 @@ from userbot import LogicWorker, bot
 
 def register(**args):
     """ Register a new event. """
-    pattern = args.get('pattern', None)
-    disable_edited = args.get('disable_edited', False)
-    ignore_unsafe = args.get('ignore_unsafe', False)
-    unsafe_pattern = r'^[^/!#@\$A-Za-z]'
-    group_only = args.get('group_only', False)
-    disable_errors = args.get('disable_errors', False)
-    permit_sudo = args.get('permit_sudo', False)
-    if pattern is not None and not pattern.startswith('(?i)'):
-        args['pattern'] = '(?i)' + pattern
-
-    if "disable_edited" in args:
-        del args['disable_edited']
-
-    if "ignore_unsafe" in args:
-        del args['ignore_unsafe']
-
-    if "group_only" in args:
-        del args['group_only']
-
-    if "disable_errors" in args:
-        del args['disable_errors']
-
-    if "permit_sudo" in args:
-        del args['permit_sudo']
-
-    if pattern:
-        if not ignore_unsafe:
-            args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
-
     def decorator(func):
+        pattern = args.get('pattern', None)
+        disable_edited = args.get('disable_edited', False)
+        ignore_unsafe = args.get('ignore_unsafe', False)
+        unsafe_pattern = r'^[^/!#@\$A-Za-z]'
+        group_only = args.get('group_only', False)
+        disable_errors = args.get('disable_errors', False)
+        permit_sudo = args.get('permit_sudo', False)
+
+        if pattern is not None and not pattern.startswith('(?i)'):
+            args['pattern'] = '(?i)' + pattern
+
+        if "disable_edited" in args:
+            del args['disable_edited']
+
+        if "ignore_unsafe" in args:
+            del args['ignore_unsafe']
+
+        if "group_only" in args:
+            del args['group_only']
+
+        if "disable_errors" in args:
+            del args['disable_errors']
+
+        if "permit_sudo" in args:
+            del args['permit_sudo']
+
+        if pattern:
+            if not ignore_unsafe:
+                args['pattern'] = pattern.replace('^.', unsafe_pattern, 1)
+
+
         async def wrapper(check):
             if group_only and not check.is_group:
                 await check.respond("`Are you sure this is a group?`")
                 return
-
-            # Check if the sudo is an admin already, if yes, we can avoid acting to his command.
-            #If his admin was limited, its his problem.
+            # Check if the sudo is an admin already,
+            # if yes, we can avoid acting to his command.
+            # If his admin was limited, its his problem.
             if permit_sudo and not check.out:
                 if check.sender_id in LogicWorker:
                     async for user in check.client.iter_participants(
@@ -84,53 +86,41 @@ def register(**args):
             except KeyboardInterrupt:
                 pass
             except BaseException:
-
                 # Check if we have to disable it.
                 # If not silence the log spam on the console,
                 # with a dumb except.
-
                 if not disable_errors:
-                    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
-                    text = "**Sorry, I encountered a error!**\n"
-                    link = "[https://t.me/userbot_support](Userbot Support Chat)"
-                    text += "If you wanna you can report it"
-                    text += f"- just forward this message to {link}.\n"
-                    text += "I won't log anything except the fact of error and date\n"
-
-                    ftext = "\nDisclaimer:\nThis file uploaded ONLY here, "
-                    ftext += "we logged only fact of error and date, "
-                    ftext += "we respect your privacy, "
-                    ftext += "you may not report this error if you've "
-                    ftext += "any confidential data here, noone will see your data\n\n"
-                    ftext += "--------BEGIN USERBOT TRACEBACK LOG--------"
-                    ftext += "\nDate: " + date
-                    ftext += "\nGroup ID: " + str(check.chat_id)
-                    ftext += "\nSender ID: " + str(check.sender_id)
-                    ftext += "\n\nEvent Trigger:\n"
-                    ftext += str(check.text)
-                    ftext += "\n\nTraceback info:\n"
-                    ftext += str(format_exc())
-                    ftext += "\n\nError text:\n"
-                    ftext += str(sys.exc_info()[1])
-                    ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
-
-                    command = "git log --pretty=format:\"%an: %s\" -5"
-
-                    ftext += "\n\n\nLast 5 commits:\n"
-
                     process = await asyncsubshell(
-                        command,
+                        "git log --pretty=format:\"%an: %s\" -5",
                         stdout=asyncsub.PIPE,
                         stderr=asyncsub.PIPE)
                     stdout, stderr = await process.communicate()
-                    result = str(stdout.decode().strip()) \
-                        + str(stderr.decode().strip())
 
-                    ftext += result
+                    link = "[https://t.me/userbot_support](Userbot Support Chat)"
+
+                    text = f"**Sorry, I encountered a error!**\n \
+                            If you want, you can report this to {link}.\n \
+                            I won't log anything except for the exception and date. \n\n \
+                            Disclaimer:\n \
+                            This file uploaded ONLY here, we logged only fact of error \
+                            and date, we respect your privacy. You may not report this \
+                            if this containing any non-disclosure data, no one will \
+                            see your data. \n\n"
+
+                    traceback = f"--------BEGIN USERBOT TRACEBACK LOG--------\n \
+                                Date: {strftime('%Y-%m-%d %H:%M:%S', gmtime())} \n \
+                                Group ID: {str(check.chat_id)} \n \
+                                Sender ID: {str(check.sender_id)} \n \
+                                Event Trigger: \n{str(check.text)} \
+                                Traceback Info: \n {str(format_exc())} \
+                                Error Text: \n {str(sys.exc_info()[1])} \n\n \
+                                -------- END USERBOT TRACEBACK LOG-------- \n\n \
+                                Last 5 commits: \n \
+                                {str(stdout.decode().strip())} \
+                                {str(stderr.decode().strip())}"
 
                     file = open("error.log", "w+")
-                    file.write(ftext)
+                    file.write(traceback)
                     file.close()
 
                     await check.client.send_file(
