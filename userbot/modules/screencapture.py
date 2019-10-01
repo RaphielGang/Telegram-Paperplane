71659ce3dda9a0dd260e69c2172cd536d9d091cf
rupansh-arch <rupanshsekar@hotmail.com>
2019-10-01 22:02:51 +0530

    userbot: modules: screencapture: rewrite non-OSSRPL modules
    kek
    
    Signed-off-by: rupansh-arch <rupanshsekar@hotmail.com>

--- userbot/modules/screencapture.py
+++ userbot/modules/screencapture.py
@@ -1,10 +1,9 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2019 The Raphielscape Company LLC, Rupansh Sekar.
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-# The entire source code is OSSRPL except 'screencapture' which is MPL
-# License: MPL and OSSRPL
+# The entire source code is OSSRPL
 """ Userbot module for ScreenshotLayer API """
 
 import os
@@ -15,43 +14,39 @@ from userbot import CMD_HELP, SCREENSHOT_LAYER_ACCESS_KEY
 from userbot.events import register
 
 
+BASEURL = "https://api.screenshotlayer.com/api/capture"
+
+
 @register(pattern=r"^.screencapture (.*)", outgoing=True)
 async def capture(url):
     """ For .screencapture command, capture a website and send the photo. """
     if SCREENSHOT_LAYER_ACCESS_KEY is None:
-        await url.edit(
-            "Need to get an API key from https://screenshotlayer.com\
-            /product \nModule stopping!")
+        await url.edit("Invalid Api Key")
         return
-    await url.edit("Processing ...")
-    sample_url = "https://api.screenshotlayer.com/api/capture"
-    sample_url += "?access_key={}&url={}&fullpage={}&format={}&viewport={}"
-    input_str = url.pattern_match.group(1)
-    response_api = get(
-        sample_url.format(SCREENSHOT_LAYER_ACCESS_KEY, input_str, "1", "PNG",
-                          "2560x1440"),
-        stream=True,
-    )
-    content_type = response_api.headers["content-type"]
+    imglink = url.pattern_match.group(1)
+    parameterz = {"access_key": SCREENSHOT_LAYER_ACCESS_KEY, "url": imglink, "fullpage": "1",
+                  "format": "PNG", "viewport": "2560x1440"}
+    resp = get(BASEURL, stream=True, params=parameterz)
+    content_type = resp.headers["content-type"]
     if "image" in content_type:
-        temp_file_name = "screencapture.png"
-        with open(temp_file_name, "wb") as file:
-            for chunk in response_api.iter_content(chunk_size=128):
+        temp = "screencapture.png"
+        with open(temp, "wb") as file:
+            for chunk in resp.iter_content(chunk_size=128):
                 file.write(chunk)
         try:
             await url.client.send_file(
                 url.chat_id,
-                temp_file_name,
-                caption=input_str,
+                temp,
+                caption=imglink,
                 force_document=True,
                 reply_to=url.message.reply_to_msg_id,
             )
             await url.delete()
         except BaseException:
-            await url.edit(response_api.text)
-        os.remove(temp_file_name)
+            await url.edit(resp.text)
+        os.remove(temp)
     else:
-        await url.edit(response_api.text)
+        await url.edit(resp.text)
 
 
 CMD_HELP.update({
