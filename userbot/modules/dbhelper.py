888d93d8eb5b795fd4ce61d9f814a215f0f3e0dc
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2019-10-09 18:56:25 +0700

    modules / dbhelper : Refactor
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- userbot/modules/dbhelper.py
+++ userbot/modules/dbhelper.py
@@ -432,24 +432,23 @@ async def get_fban():
 async def add_chat_fban(chatid):
     if await is_fban(chatid) is True:
         return False
-    else:
-        MONGO.fban.insert_one({'chatid': chatid})
+
+    MONGO.fban.insert_one({'chatid': chatid})
 
 
 async def remove_chat_fban(chatid):
     if await is_fban(chatid) is False:
         return False
-    else:
-        MONGO.fban.delete_one({'chatid': chatid})
-        return True
+
+    MONGO.fban.delete_one({'chatid': chatid})
+    return True
 
 
 async def is_fban(chatid):
     if not MONGO.fban.find_one({"chatid": chatid}):
-        print("FAILED on fed")
         return False
-    else:
-        return True
+
+    return True
 
 
 # Gbans
@@ -459,25 +458,24 @@ async def get_gban():
 
 async def add_chat_gban(chatid):
     if await is_gban(chatid) is True:
-        print("FAILED")
         return False
-    else:
-        MONGO.gban.insert_one({'chatid': chatid})
+
+    MONGO.gban.insert_one({'chatid': chatid})
 
 
 async def remove_chat_gban(chatid):
     if await is_gban(chatid) is False:
         return False
-    else:
-        MONGO.gban.delete_one({'chatid': chatid})
-        return True
+
+    MONGO.gban.delete_one({'chatid': chatid})
+    return True
 
 
 async def is_gban(chatid):
     if not MONGO.gban.find_one({"chatid": chatid}):
         return False
-    else:
-        return True
+
+    return True
 
 
 # Time
