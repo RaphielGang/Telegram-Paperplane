b396c75c2c4c481e009610f1669a6dbe189da466
nysascape <nysadev@raphielgang.org>
2020-02-18 21:30:26 +0000

    main: Better structure errors, change support group
    Signed-off-by: nysascape <nysadev@raphielgang.org>
    Change-Id: Iebb51fd99185e1495d9117727c572f5da0b1e9c9

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -13,8 +13,8 @@ from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 from userbot import LOGS, bot, PAPERPLANE_VERSION
 from userbot.modules import ALL_MODULES
 
-INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
-             '\n  Tip: Use Country Code along with No.' \
+INVALID_PH = '\nERROR: The phone no. entered is INVALID' \
+             '\n  Tip: Use country code (eg +44) along with No.' \
              '\n       Recheck your Phone Number'
 
 try:
@@ -26,9 +26,9 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("Your Bot is alive! Test it by typing .alive on any chat."
-          " Should you need assistance, head to https://t.me/userbot_support")
-LOGS.info("Your Bot Version is %s", PAPERPLANE_VERSION)
+LOGS.info("Paperplane is alive! Test it by typing .alive on any chat."
+          " Should you need assistance, head to https://t.me/tgpaperplane")
+LOGS.info("Your bot version is %s", PAPERPLANE_VERSION)
 
 CI_TEST = environ.get("CI", None)
 if CI_TEST:
