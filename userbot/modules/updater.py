5157fb8200bf131813af1c1a25388cd4b69506d1
nysascape <nysadev@raphielgang.org>
2020-02-22 12:16:01 +0000

    userbot / treewide: Refactor licence from 1.c to 1.d
    Signed-off-by: nysascape <nysadev@raphielgang.org>
    Change-Id: I9aefa5c6b4d9ab1e159b338dc4e53e403631feb2

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
 """
