b80c8fce05413490ef4a1c724a14fa5a166d7be7
DragonightFury <37414171+DragonightFury@users.noreply.github.com>
2020-04-27 12:51:48 +0300

    generate_string_session: Better prints
    Co-Authored-By: Geg Zakaryan <zakaryan.2004@outlook.com>

--- generate_string_session.py
+++ generate_string_session.py
@@ -10,6 +10,6 @@ API_KEY = input("API_KEY: ")
 API_HASH = input("API_HASH: ")
 
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-    print("Here is your userbot srting, copy it to config.env!")
+    print("This is your string session, be careful with it and don't share it with anyone else!")
     print("")
     print(client.session.save())
