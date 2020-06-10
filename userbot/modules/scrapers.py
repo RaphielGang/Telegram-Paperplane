fe63f47da9989f42a2e4908b0890709fbb61d9ba
Hitalo <hitalomarquete331@gmail.com>
2020-06-10 11:41:02 -0300

    Merged reverse module with scrapers

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -5,9 +5,16 @@
 #
 """ Userbot module containing various scrapers. """
 
+import io
 import asyncio
 import os
 import time
+import urllib
+import requests
+import re
+
+from PIL import Image
+from bs4 import BeautifulSoup
 from re import findall
 from shutil import rmtree
 from urllib.error import HTTPError
@@ -27,9 +34,16 @@ from youtube_dl.utils import (DownloadError, ContentTooShortError,
                               MaxDownloadsReached, PostProcessingError,
                               UnavailableVideoError, XAttrMetadataError)
 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
 from userbot.events import register
 from userbot.modules.upload_download import progress
+
+opener = urllib.request.build_opener()
+useragent = ('Mozilla/5.0 (Linux; Android 10; SM-G975F) '
+             'AppleWebKit/537.36 (KHTML, like Gecko) '
+             'Chrome/80.0.3987.149 Mobile Safari/537.36'
+             )
+opener.addheaders = [('User-agent', useragent)]
 
 # Default language to EN
 LANG = "en"
@@ -264,11 +278,6 @@ async def lang(value):
             BOTLOG_CHATID, "Default language changed to **" + LANG + "**")
 
 
-def deEmojify(inputString):
-    """ Remove emojis and other non-safe characters from string """
-    return get_emoji_regexp().sub(u'', inputString)
-
-
 @register(outgoing=True, pattern=r"^\.rip(audio|video) (.*)")
 async def download_video(v_url):
     """ For .rip command, download media from YouTube and many other sites. """
@@ -406,6 +415,125 @@ async def download_video(v_url):
         await v_url.delete()
 
 
+@register(outgoing=True, pattern=r"^.reverse(?: |$)(\d*)")
+async def okgoogle(img):
+    """ For .reverse command, Google search images and stickers. """
+    if os.path.isfile("okgoogle.png"):
+        os.remove("okgoogle.png")
+
+    message = await img.get_reply_message()
+    if message and message.media:
+        photo = io.BytesIO()
+        await bot.download_media(message, photo)
+    else:
+        return await img.edit("`Reply to photo or sticker nigger.`")
+
+    if photo:
+        await img.edit("`Processing...`")
+        try:
+            image = Image.open(photo)
+        except OSError:
+            return await img.edit('`Unsupported sexuality, most likely.`')
+        name = "okgoogle.png"
+        image.save(name, "PNG")
+        image.close()
+        # https://stackoverflow.com/questions/23270175/google-reverse-image-search-using-post-request#28792943
+        searchUrl = 'https://www.google.com/searchbyimage/upload'
+        multipart = {
+            'encoded_image': (name, open(name, 'rb')),
+            'image_content': ''
+        }
+        response = requests.post(searchUrl,
+                                 files=multipart,
+                                 allow_redirects=False)
+        fetchUrl = response.headers['Location']
+
+        if response != 400:
+            await img.edit("`Image successfully uploaded to Google. Maybe.`"
+                           "\n`Parsing source now. Maybe.`")
+        else:
+            return await img.edit("`Google told me to fuck off.`")
+
+        os.remove(name)
+        match = await ParseSauce(fetchUrl +
+                                 "&preferences?hl=en&fg=1#languages")
+        guess = match['best_guess']
+        imgspage = match['similar_images']
+
+        if guess and imgspage:
+            await img.edit(f"[{guess}]({fetchUrl})\n\n`Looking for images...`")
+        else:
+            return await img.edit("`Couldn't find anything for your uglyass.`")
+
+        if img.pattern_match.group(1):
+            lim = img.pattern_match.group(1)
+        else:
+            lim = 3
+        images = await scam(match, lim)
+        yeet = []
+        for i in images:
+            k = requests.get(i)
+            yeet.append(k.content)
+        try:
+            await img.client.send_file(entity=await
+                                       img.client.get_input_entity(img.chat_id
+                                                                   ),
+                                       file=yeet,
+                                       reply_to=img)
+        except TypeError:
+            pass
+        await img.edit(
+            f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})")
+
+
+async def ParseSauce(googleurl):
+    """Parse/Scrape the HTML code for the info we want."""
+
+    source = opener.open(googleurl).read()
+    soup = BeautifulSoup(source, 'html.parser')
+
+    results = {'similar_images': '', 'best_guess': ''}
+
+    try:
+        for similar_image in soup.findAll('input', {'class': 'gLFyf'}):
+            url = 'https://www.google.com/search?tbm=isch&q=' + \
+                urllib.parse.quote_plus(similar_image.get('value'))
+            results['similar_images'] = url
+    except BaseException:
+        pass
+
+    for best_guess in soup.findAll('div', attrs={'class': 'r5a77d'}):
+        results['best_guess'] = best_guess.get_text()
+
+    return results
+
+
+async def scam(results, lim):
+
+    single = opener.open(results['similar_images']).read()
+    decoded = single.decode('utf-8')
+
+    imglinks = []
+    counter = 0
+
+    pattern = r'^,\[\"(.*[.png|.jpg|.jpeg])\",[0-9]+,[0-9]+\]$'
+    oboi = re.findall(pattern, decoded, re.I | re.M)
+
+    for imglink in oboi:
+        counter += 1
+        if not counter >= int(lim):
+            imglinks.append(imglink)
+        else:
+            break
+
+    return imglinks
+
+
+def deEmojify(inputString):
+    """ Remove emojis and other non-safe characters from string """
+    return get_emoji_regexp().sub(u'', inputString)
+
+
 CMD_HELP.update({"scrapers": ['Scrapers',
                               " - `img` <query> lim=<n>: Do an Image Search on Google and send n results. Default is 2.\n"
                               " - `google` <query>: Search Google for query (argument or reply).\n"
@@ -414,6 +542,7 @@ CMD_HELP.update({"scrapers": ['Scrapers',
                               " - `tts` <query>: Text-to-Speech the query (argument or reply) to the saved language.\n"
                               " - `trt` <query>: Translate the query (argument or reply) to the saved language.\n"
                               " - `lang` <lang>: Changes the default language of trt and TTS modules.\n"
-                              " - `ripaudio` <url> or `ripvideo` <url>: Download videos and songs from YouTube (and [many other sites](https://ytdl-org.github.io/youtube-dl/supportedsites.html)).\n\n"
+                              " - `ripaudio` <url> or `ripvideo` <url>: Download videos and songs from YouTube (and [many other sites](https://ytdl-org.github.io/youtube-dl/supportedsites.html)).\n"
+                              " - `reverse`: Reply to a pic/sticker to revers-search it on Google Images\n\n"
                               "**All commands can be used with** `.`"]
                  })
