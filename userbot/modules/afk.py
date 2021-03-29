5854cbe5ea82aa0e9f2812478b4e0a13eeeb2563
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-03-29 13:14:27 +0530

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -30,7 +30,7 @@ async def mention_afk(mention):
         if IsAway is True:
             if mention.sender_id not in USERS:
                 await mention.reply(
-                    "Sorry! My owner is AFK!\n" "He/She said: " + await afk_reason() +
+                    "Sorry! My owner is AFK!\n" "He/She said: "\" + await afk_reason() +\"
                     ". Would ping him/her to look into the message soon😉")
                 USERS.update({mention.sender_id: 1})
                 COUNT_MSG = COUNT_MSG + 1
@@ -39,8 +39,8 @@ async def mention_afk(mention):
                     await mention.reply(
                         "My owner is still not here. "
                         "Try to ping my owner a little later. I am sorry😖."
-                        "I recall my owner saying  ```" +
-                        await afk_reason() + "```")
+                        "I recall my owner saying  ```"\" +
+                        await afk_reason() + \""```")
                     USERS[mention.sender_id] = USERS[mention.sender_id] + 1
                     COUNT_MSG = COUNT_MSG + 1
                 else:
@@ -60,7 +60,7 @@ async def afk_on_pm(afk_pm):
         if IsAway is True:
             if afk_pm.sender_id not in USERS:
                 await afk_pm.reply(
-                    "Sorry! My owner is AFK!\n" "He/She said: ```" + await afk_reason() +
+                    "Sorry! My owner is AFK!\n" "He/She said: ```"\" + await afk_reason() +\"
                     "``` I'll ping my owner to look into the message soon😉")
                 USERS.update({afk_pm.sender_id: 1})
                 COUNT_MSG = COUNT_MSG + 1
@@ -69,8 +69,8 @@ async def afk_on_pm(afk_pm):
                     await afk_pm.reply(
                         "Sorry! But my owner is still not here. "
                         "Try to ping my owner a little later. I am sorry😖."
-                        "I recall my owner saying ```" +
-                        await afk_reason() + "```")
+                        "I recall my owner saying ```" \"+
+                        await afk_reason() +\" "```")
                     USERS[afk_pm.sender_id] = USERS[afk_pm.sender_id] + 1
                     COUNT_MSG = COUNT_MSG + 1
                 else:
