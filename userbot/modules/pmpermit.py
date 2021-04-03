f49a87766c48513b148713f4c61d1d03508dd59f
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-04-04 01:19:54 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -96,7 +96,7 @@ async def permitpm(event):
                                         " which I don't like.`"
                                         " `I'mma Report Spam.`")
                     n = UNAPPROVED_MSG
-                    n = (f"You have {range(1:)} warns left.")
+                    n = (f"You have {range(1, 999)} warns left.")
                     n = ("You have 1 warn left.")
                     n = ("It's the last warning. I will block")
                     async for reply in event.client.iter_messages(
@@ -222,7 +222,7 @@ async def approvepm(apprvpm):
             await x.delete()
             
             n = UNAPPROVED_MSG
-            n = (f"You have {range(1:)} warns left.")
+            n = (f"You have {range(1, 999)} warns left.")
             n = ("You have 1 warn left.")
             n = ("It's the last warning. I will block")
             async for reply in apprvpm.client.iter_messages(
