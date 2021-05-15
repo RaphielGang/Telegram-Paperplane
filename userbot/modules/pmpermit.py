fcb82ec49426e272bcdbbf644a617923be065167
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-05-15 23:29:40 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -49,7 +49,8 @@ UNAPPROVED_MSG = PM_PERMIT_MSG or (
 MAX_MSG = MAX_FLOOD_IN_PM or 5
 # =================================================================
 
-async def del_in(pp_event, seconds):
+async def del_in(pp_event, seconds=None):
+    seconds = seconds or 5
     await asyncio.sleep(seconds)
     return await pp_event.delete()
 
