710678b7314f65f112d3f68e9dffdedf21efacbc
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-05-23 23:11:18 +0530

    Update dbhelper.py

--- userbot/modules/dbhelper.py
+++ userbot/modules/dbhelper.py
@@ -253,16 +253,16 @@ async def approval(userid):
 
     
 async def autoapproval(userid):
-    to_check = MONGO.pmpermit.find_one({'autoapprv': userid})
+    #to_check = MONGO.pmpermit.find_one({'autoapprv': userid})
 
-    if to_check is None:
+    #if to_check is None:
         MONGO.pmpermit.insert_one({'autoapprv': userid, 'autoapproval': False})
 
         return False
-    elif to_check['autoapproval'] is False:
-        return False
-    elif to_check['autoapproval'] is True:
-        return True    
+    #elif to_check['autoapproval'] is False:
+        #return False
+    #elif to_check['autoapproval'] is True:
+        #return True    
 
 async def approve(userid):
     if await approval(userid) is True:
