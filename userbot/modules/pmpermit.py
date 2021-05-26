7dfbf745af65bbdca7d714d974e3f89b918eb32d
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-05-26 15:39:56 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -90,16 +90,18 @@ async def permitpm(event):
                     LASTMSG.update({event.chat_id: event.text})
                 if await notif_state():
                     if BOTLOG:
-                        name = await event.client.get_entity(event.chat_id)
-                        name0 = str(name.first_name)
-                        async for log_message in event.client.iter_messages(
-                            BOTLOG_CHATID, from_user="me", search="#INCOMMING_PM\n["+ name0+ "](tg://user?id="+ str(event.chat_id)+ ")"+ " is waiting in your PM"):
-                            await log_message.delete()
-                        await event.client.send_message(
-                            BOTLOG_CHATID,
-                            "#INCOMMING_PM\n["+ name0+ "](tg://user?id="+ str(event.chat_id)+ ")"+ 
-                            " is waiting in your PM"
-                        )
+                        if event.chat_id in LASTMSG:
+                            name = await event.client.get_entity(event.chat_id)
+                            name0 = str(name.first_name)
+                            async for log_message in event.client.iter_messages(
+                                BOTLOG_CHATID, from_user="me", search="#INCOMMING_PM\n["+ name0+ "](tg://user?id="+ str(event.chat_id)+ ")"+ " is waiting in your PM"):
+                                continue
+                        else:
+                            await event.client.send_message(
+                                BOTLOG_CHATID,
+                                "#INCOMMING_PM\n["+ name0+ "](tg://user?id="+ str(event.chat_id)+ ")"+ 
+                                " is waiting in your PM"
+                            )
                     
                 if await notif_state() is False:
                     await event.client.send_read_acknowledge(event.chat_id)
