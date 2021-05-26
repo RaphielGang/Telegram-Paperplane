b3c90486c7d9b8728b16e26b9a20d8fa558453d2
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-05-26 15:33:05 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -92,9 +92,9 @@ async def permitpm(event):
                     if BOTLOG:
                         name = await event.client.get_entity(event.chat_id)
                         name0 = str(name.first_name)
-                        async for message in event.client.iter_messages(
+                        async for log_message in event.client.iter_messages(
                             BOTLOG_CHATID, from_user="me", search="#INCOMMING_PM\n["+ name0+ "](tg://user?id="+ str(event.chat_id)+ ")"+ " is waiting in your PM"):
-                            continue
+                            await log_message.delete()
                         await event.client.send_message(
                             BOTLOG_CHATID,
                             "#INCOMMING_PM\n["+ name0+ "](tg://user?id="+ str(event.chat_id)+ ")"+ 
