410cfc1ad32d3b5645a03b5ee0d395b6fd6e6e34
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-05-27 10:37:41 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -75,7 +75,7 @@ async def permitpm(event):
                         await event.respond(UNAPPROVED_MSG)
                         MONGO.userbot.pmpermit.insert_one({'prev_msg': event.chat_id})
                 else:
-                    async for message in event.client.iter_messages(event.chat_id, from_user="me", search=UNAPPROVED_MSG):
+                    async for message in event.client.iter_messages(event.chat_id, from_user="me", search=[UNAPPROVED_MSG, file=PM_PERMIT_IMAGE]):
                         await message.delete()
                             
 
