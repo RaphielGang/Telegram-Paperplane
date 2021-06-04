67c643bcc69f581e0c81be31d569fdc44b04408f
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-06-04 20:35:50 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -171,6 +171,7 @@ os.chmod('bin/megadown', 0o755)
 os.chmod('bin/cmrudl', 0o755)
 
 # Global Variables
+COMMAND_TRIGGER = os.environ.get("COMMAND_TRIGGER") or None
 COUNT_MSG = 0
 MAX_FLOOD_IN_PM = os.environ.get("MAX_FLOOD_IN_PM") or None
 PM_PERMIT_MSG = os.environ.get("PM_PERMIT_TEXT") or None
