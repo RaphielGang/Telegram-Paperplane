6c18015f219ea385622be55a9b67c30cac9f3d78
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-06-11 20:16:46 +0530

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -79,10 +79,10 @@ CMD_HELP.update(
     {
         "alive": [
             "Alive",
-            " - `.alive`: Check if your bot is running."
-            " - `setapic`: Set picture for your alive message."
-            " - `getapic`: List all the pictures you have set."
-            " - `delapic`: Delete all the pictures you have set."
+            " - `.alive`: Check if your bot is running.\n"
+            " - `setapic`: Set picture for your alive message.\n"
+            " - `getapic`: List all the pictures you have set.\n"
+            " - `delapic`: Delete all the pictures you have set.\n"
         ]
     }
 )
