130d9ddda9e1f8b37ad3c9df865b29dab63b7515
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-06-18 21:32:31 +0530

    Update telegraph.py

--- userbot/modules/telegraph.py
+++ userbot/modules/telegraph.py
@@ -1,1 +1,62 @@
+import time
+import datetime
 
+from telegraph import Telegraph, upload_file, exceptions
+from .exceptions import TelegraphException
+
+from userbot import BOTLOG, BOTLOG_CHATID
+
+@register(pattern=r"telegraph media|tgm", outgoing=True)
+@grp_exclude()
+async def telegraph(event):
+    """Gives telegraph link of a given media.
+       No text."""
+    
+    media = await event.get_reply_message()
+    downloaded = await event.client.download_media(media)
+    await event.edit("Starting...")
+    
+    telegraph = Telegraph()
+    account = telegraph.create_account(short_name="Paperplane")
+    auth_url = account["author_url"]
+    
+    if not downloaded.ends_with(
+        ".jpg", ".jpeg", ".png", ".gif" ".mp4"
+    ):
+        await event.edit("I don't support that media.")
+        return
+      
+    
+    await event.edit("Created telegraph account.")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            "Created an account in telegraph. "
+            "[Link]"
+            "("
+            + auth_url
+            ")"
+        ) 
+    
+    
+    try:
+      sttime = datetime.now()
+      tlg_url = await upload_file(downloaded)
+    except TelegraphException as error:
+      await event.edit("Oh no! I got an error")
+      time.sleep(1)
+      await event.edit(error)
+      return
+    
+    entime = datetime.now()
+    time_passed = entime - sttime
+    time_taken = time_passed.seconds
+    
+    await event.edit("• Your telegraph link is here: "
+                     f"[link](tlg_url)"
+                     "\n" 
+                     f"• Uploaded in "
+                     + time_taken
+               )
+    
+    
