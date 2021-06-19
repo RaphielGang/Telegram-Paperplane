898453f9c7b7c32c2a6c521453f9d1640b76028a
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-06-19 14:19:48 +0530

    Update pokedex.py

--- userbot/modules/pokedex.py
+++ userbot/modules/pokedex.py
@@ -5,7 +5,7 @@ from userbot.events import register, grp_exclude
 @register(outgoing=True, pattern="pokedex")
 @grp_exclude()
 async def pokedex(pallet):
-    pokedex = pokedex.Pokedex()
+    pokedex = pokedex.Pokedex(version = 'v1')
     pokemon_name = str(pallet.text[9: ])
     pokemon = pokedex.get_pokemon_by_name(pokemon_name)
     await pallet.edit(str(pokemon))
