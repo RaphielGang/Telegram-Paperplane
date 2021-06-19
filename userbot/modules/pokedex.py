01bb10f98849a567a7e7830c1df1afd16568f384
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-06-19 17:01:52 +0530

    Update pokedex.py

--- userbot/modules/pokedex.py
+++ userbot/modules/pokedex.py
@@ -8,4 +8,34 @@ async def pokedex(dexter):
     pokedex = dex.Pokedex(version = 'v1')
     pokemon_name = str(dexter.text[9: ])
     pokemon = pokedex.get_pokemon_by_name(pokemon_name)
-    await dexter.edit(str(pokemon))
+    pokedetails = str(pokemon)
+    
+  #================= POKEMON STATS ==================== #   
+    Name = pokedetails['name']                          #
+    Species = pokedetails['species']                    #
+    Type = pokedetails['types']                         #
+    Ability = pokedetails['abilities']                  #                   
+    EggGroup = pokedetails['eggGroups']['normal']       #
+    Stage = pokedetails['family']['evolutionStage']     #
+    Evolution = pokedetails['family']['evolutionLine']  #
+    Gender = pokedetails['gender']                      #
+    Height = pokedetails['height']                      #
+    Weight = pokedetails['weight']                      #
+    Sprite = pokedetails['sprite']                      #
+    Description = pokedetails['description']            #
+  #==================================================== #
+  
+    message = (
+        f"**Name: **{Name}"
+        f"**Type: **{Type}"
+        f"**Abilities: **{Ability}"
+        f"**Gender: **{Gender}"
+        f"**Height: **{Height}"
+        f"**Weight: **{Weight}"
+        f"**Stage: **{Stage}"
+        f"**Evolution: **{Evolution}"
+        f"**EggGroup: **{EggGroup}"
+        f"**Description: **__{Description}__"
+    )
+    
+    await dexter.reply(message)
