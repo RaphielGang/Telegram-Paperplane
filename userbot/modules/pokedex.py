910da0f43b442c0d180c82d91cd756fdc834e5bb
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-06-19 18:06:35 +0530

    Update pokedex.py

--- userbot/modules/pokedex.py
+++ userbot/modules/pokedex.py
@@ -7,34 +7,34 @@ from userbot.events import register, grp_exclude
 async def pokedex(dexter):
     pokemon_name = str(dexter.text[9: ])
     pokemon = dex.get(name=pokemon_name)
-    pokemon_details = str(pokemon.pokemon)
-    """  
+
+ 
   #================= POKEMON STATS ================= #   
-    Name = pokedetails[2]                            #
-    Species = pokedetails[3]                         #
-    Type = pokedetails[4]                            #
-    Ability = pokedetails[5]                         #                   
-    EggGroup = pokedetails[6]                        #
-    Stage = pokedetails[10]                          #
-    Evolution = pokedetails[10]                      #
-    Gender = pokedetails[7]                          #
-    Height = pokedetails[8]                          #
-    Weight = pokedetails[9]                          #
-    Sprite = pokedetails[18]                         #
-    Description = pokedetails[19]                    #
+    Name = str(pokemon.name)                         #
+    Height = str(pokemon.height)                     #
+    Weight = str(pokemon.weight)                     #
+    Exp = str(pokemon.base_experience)               #                   
+    Ability = str(pokemon.abilities(name))           #
+                                                     #
+                                                     #
+                                                     #
+                                                     #
+                                                     #
+                                                     #
+                                                     #
   #================================================= #
   
     message = (
-        f"**Name: **{Name}"
-        f"**Type: **{Type}"
-        f"**Abilities: **{Ability}"
-        f"**Gender: **{Gender}"
-        f"**Height: **{Height}"
-        f"**Weight: **{Weight}"
-        f"**Stage: **{Stage}"
-        #f"**Evolution: **{Evolution}"
-        f"**EggGroup: **{EggGroup}"
-        f"**Description: **__{Description}__"
+        f"**Name: **{Name}\n"
+        f"**Type: **{Type}\n"
+        f"**Abilities: **{Ability}\n"
+       #f"**Gender: **{Gender}"
+        f"**Height: **{Height}\n"
+        f"**Weight: **{Weight}\n"
+       #f"**Stage: **{Stage}"
+       #f"**Evolution: **{Evolution}"
+       #f"**EggGroup: **{EggGroup}"
+       #f"**Description: **__{Description}__"
     )
-    """
-    await dexter.edit(pokemon_details)
+
+    await dexter.edit(message)
