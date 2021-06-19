7982066bd23073daf68226dbd573ba4b9d646a17
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-06-19 14:27:10 +0530

    Update pokedex.py

--- userbot/modules/pokedex.py
+++ userbot/modules/pokedex.py
@@ -7,5 +7,5 @@ from userbot.events import register, grp_exclude
 async def pokedex(dexter):
     pokedex = dex.Pokedex(version = 'v1')
     pokemon_name = str(dexter.text[9: ])
-    pokemon = dexter.get_pokemon_by_name(pokemon_name)
+    pokemon = dex.get_pokemon_by_name(pokemon_name)
     await dexter.edit(str(pokemon))
