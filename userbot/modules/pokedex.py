1a33c222b7642da776ee729b7abcd79807ab1972
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-06-19 13:45:08 +0530

    Update pokedex.py

--- userbot/modules/pokedex.py
+++ userbot/modules/pokedex.py
@@ -1,1 +1,10 @@
+import pypokedex
 
+from userbot import register, grp_exclude
+
+@register(outgoing=True, pattern="pokedex")
+@grp_exclude()
+async def pokedex(pallet):
+    pokemon_name = str(pallet.text[8: ])
+    pokemon = pypokedex.get(name=pokemon_name)
+    await pallet.edit(pokemon.types)
