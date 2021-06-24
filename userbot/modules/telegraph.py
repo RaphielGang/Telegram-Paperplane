8945cf58a62f483d15f69c6cab718c1f11adbbd5
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-06-24 11:58:18 +0530

    Update telegraph.py

--- userbot/modules/telegraph.py
+++ userbot/modules/telegraph.py
@@ -6,7 +6,7 @@ from telegraph import Telegraph, upload_file, exceptions
 from userbot import BOTLOG, BOTLOG_CHATID
 from userbot.events import register, grp_exclude
 
-@register(pattern="t(?:elegraph|gm$) media$", outgoing=True)
+@register(pattern="tgm", outgoing=True)
 @grp_exclude()
 async def telegraph(media):
     """Gives telegraph link of a given media.
@@ -83,7 +83,7 @@ async def telegraph(media):
                )
     
 
-@register(pattern="t(?:elegraph|gt$) text$", outgoing=True)
+@register(pattern="tgt$", outgoing=True)
 @grp_exclude()
 async def telegraph(text):
     """Gives the telegraph link of text.
