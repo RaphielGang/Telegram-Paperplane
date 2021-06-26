2bc93bef16ea71c3e85f12f0b6fe070630447ca5
Ashdroid <77984053+ashdroid4@users.noreply.github.com>
2021-06-26 15:38:35 +0530

    Update chatbot.py

--- userbot/modules/chatbot.py
+++ userbot/modules/chatbot.py
@@ -25,7 +25,7 @@ async def usersetter(ai):
 
 @register(outgoing=True, pattern="rmai$")
 @grp_exclude()
-async def usersetter(ai):
+async def userremover(ai):
     """Will assign the ai to a replied user"""
     replied_msg = await ai.get_reply_message()
     user_id = replied_msg.sender.id
@@ -42,7 +42,6 @@ async def usersetter(ai):
         time.sleep(5)
         await x.delete()
 
-
 @register(incoming=True, disable_errors=True)
 @grp_exclude()
 async def aiworker(ai):
@@ -53,12 +52,47 @@ async def aiworker(ai):
     search = MONGO.chatbot.find_one({"user": message_sender, "chat": ai.chat_id})
 
     if search:
-        client = randomstuff.AsyncClient(api_key=RANDOMSTUFF_API_KEY)
+        client = randomstuff.AsyncClient(api_key=RANDOMSTUFF_API_KEY, suppress_warnings=True)
         response = await client.get_ai_response(
-            message = message,
-            master = "Lee",
-            bot = "Maple"
+            message=message,
+            master="Lee",
+            bot="Maple"
             )
 
         await ai.reply(f"{response}")
 
+
+@register(outgoing=True, pattern="image (.*)$")
+@grp_exclude()
+async def picturesender(pic):
+    """Will return images of the given argument"""
+    argv = pic.pattern_match.group(1)
+    args = ["aww", "duck", "dog", "cat", "memes", "dankmemes", "holup", "art", "harrypottermemes", "facepalm", "any"]
+
+    if argv not in args:
+        await pic.edit(
+            "Choose the correct image type from the following:\n"
+            "`any`, `art`, `aww, `cat`, `dankmemes`, `dog`, `duck`, `facepalm`, `harrypottermemes`, `holup`, `memes`"
+            return
+    else:
+        client = randomstuff.AsyncClient(api_key=RANDOMSTUFF_API_KEY, suppress_warnings=True)
+        pics = await client.get_image(type=argv)
+        await pic.reply(file=pics)
+
+@register(outgoing=True, pattern="waifu (.*)$")
+@grp_exclude()
+async def picturesender(pic):
+    """Will return images of waifus"""
+    argv = pic.pattern_match.group(1)
+    args = ["waifu", "neko", "shinobu", "megumin", "bully", "cuddle"]
+
+    if argv not in args:
+        await pic.edit(
+            "Choose the correct type from the following:\n"
+            "`bully`, `cuddle`, `megumin`, `neko`, `shinobu`, `waifu`"
+            return
+    else:
+        client = randomstuff.AsyncClient(api_key=RANDOMSTUFF_API_KEY, suppress_warnings=True)
+        pics = await client.get_waifu(type=argv)
+
+        await pic.reply(file=pics)
