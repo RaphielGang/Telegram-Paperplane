345acb2cffdd5195eb4e79f08873226d324b9a5d
Ashdroid <77984053+ashdroid4@users.noreply.github.com>
2021-06-26 10:28:40 +0530

    Update chatbot.py

--- userbot/modules/chatbot.py
+++ userbot/modules/chatbot.py
@@ -1,4 +1,5 @@
 import randomstuff
+import time
 
 from userbot.events import register, grp_exclude
 from userbot import RANDOMSTUFF_API_KEY, MONGO
@@ -10,10 +11,36 @@ async def usersetter(ai):
     """Will assign the ai to a replied user"""
     replied_msg = await ai.get_reply_message()
     user_id = replied_msg.sender.id
+    
+    search = MONGO.chatbot.find_one({"user": user_id, "chat": ai.chat_id})
 
-    MONGO.ai.insert_one({"user": user_id, "chat": ai.chat_id})
+    if search:
+        x = await ai.edit("I am already responding to this user.")
+        time.sleep(5)
+        return await x.delete()
+    else:
+        MONGO.chatbot.insert_one({"user": user_id, "chat": ai.chat_id})
 
-    await ai.edit("Hi")
+    await ai.edit("Hello!")
+
+@register(outgoing=True, pattern="rmai$")
+@grp_exclude()
+async def usersetter(ai):
+    """Will assign the ai to a replied user"""
+    replied_msg = await ai.get_reply_message()
+    user_id = replied_msg.sender.id
+    
+    search = MONGO.chatbot.find_one({"user": user_id, "chat": ai.chat_id})
+
+    if search:
+        x = await ai.edit("Nope, I don't know this user.")
+        time.sleep(5)
+        return await x.delete()
+    else:
+        MONGO.chatbot.delete_one({"user": user_id, "chat": ai.chat_id})
+        x = await ai.edit("I will not respond to this user.")
+        time.sleep(5)
+        await x.delete()
 
 
 @register(incoming=True, disable_errors=True)
@@ -23,11 +50,15 @@ async def aiworker(ai):
     message = ai.message.text
     message_sender = ai.sender.id
 
-    search = MONGO.ai.find_one({"user": message_sender, "chat": ai.chat_id})
+    search = MONGO.chatbot.find_one({"user": message_sender, "chat": ai.chat_id})
 
     if search:
-        client = randomstuff.Client(api_key=RANDOMSTUFF_API_KEY)
-        response = client.get_ai_response(message)
+        client = randomstuff.AsyncClient(api_key=RANDOMSTUFF_API_KEY)
+        response = client.get_ai_response(
+            message = message
+            master = "Lee"
+            bot = "Maple"
+            )
 
         await ai.reply(f"{response}")
 
