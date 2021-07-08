26c2a290ad29adee39fd8bc249ad4fb28b586508
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-07-08 18:32:59 +0400

    fban_gban: Rewrite, fix bugs, create helpers
    * Mostly rewritten FBan/GBan, fixing bugs and cleaning up code.
    * Moved some helper functions to helpers.py and added a new get_user_and_reason_from_event(event) helper.
    * Other small changes
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- userbot/modules/fban_gban.py
+++ userbot/modules/fban_gban.py
@@ -7,8 +7,6 @@
 
 import asyncio
 
-from telethon.tl.types import MessageEntityMentionName
-
 from userbot import CMD_HELP, bot, is_mongo_alive, is_redis_alive
 from userbot.events import register, grp_exclude
 from userbot.modules.dbhelper import (
@@ -19,116 +17,85 @@ from userbot.modules.dbhelper import (
     remove_chat_fban,
     remove_chat_gban,
 )
+from userbot.modules import helpers
 
 
-@register(outgoing=True, pattern="^.gban")
+@register(outgoing=True, pattern="^.gban(?: |$)([^\s]+)(?: |$)(.*)")
 @grp_exclude()
 async def gban_all(msg):
     if not is_mongo_alive() or not is_redis_alive():
         await msg.edit("`Database connections failing!`")
         return
-    textx = await msg.get_reply_message()
-    if textx:
-        try:
-            banreason = banreason.join(msg.text.split(" ")[1:])
-        except TypeError:
-            banreason = "[paperplane] GBan"
-    else:
-        banid = msg.text.split(" ")[1]
-        if banid.isnumeric():
-            # if its a user id
-            banid = int(banid)
-        else:
-            # deal wid the usernames
-            if msg.message.entities is not None:
-                probable_user_mention_entity = msg.message.entities[0]
+    banreason = "[paperplane] "
 
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                banid = probable_user_mention_entity.user_id
-        try:
-            banreason = banreason.join(msg.text.split(" ")[2:])
-        except TypeError:
-            banreason = "[paperplane] GBan"
-    if not textx:
+    user, reason = await helpers.get_user_and_reason_from_event(msg)
+    if not user:
         await msg.edit(
-            "Reply message missing! Might fail on many bots! Still attempting to Gban!"
+            "`Missing user to GBan! Either reply to message, mention the user or specify a valid ID!`"
         )
-        # Ensure User Read the warning
-        await asyncio.sleep(1)
-    x = await get_gban()
+        return
+    banid = user.id
+    banreason += reason if reason else "GBan"
+    message = ""
+
+    textx = await msg.get_reply_message()
+    if not textx:
+        message += "`Reply message missing! Might fail on some bots! Still attempting to GBan!`"
+        await msg.edit(message)
+
+    banlist = await get_gban()
     count = 0
-    banlist = []
-    for i in x:
-        banlist.append(i["chatid"])
-    for banbot in banlist:
+    banid_list = [row["chatid"] for row in banlist]
+    for banbot in banid_list:
         async with bot.conversation(banbot) as conv:
             if textx:
-                c = await msg.forward_to(banbot)
-                await c.reply("/id")
+                await msg.forward_to(banbot)
             await conv.send_message(f"/gban {banid} {banreason}")
-            await conv.get_response()
-            await bot.send_read_acknowledge(conv.chat_id)
             count += 1
-            # We cant see if he actually Gbanned. Let this stay for now
-            await msg.edit("`Gbanned on " + str(count) + " bots!`")
-            await asyncio.sleep(0.2)
+
+    # We can't see if the user is actually Gbanned.
+    message = f"\n`Gbanned on {str(count)} bot(s)! Paperplane doesn't check if the GBans were successful.`"
+    await msg.edit(message)
 
 
-@register(outgoing=True, pattern="^.fban")
+@register(outgoing=True, pattern="^.fban(?: |$)([^\s]+)(?: |$)(.*)")
 @grp_exclude()
 async def fedban_all(msg):
     if not is_mongo_alive() or not is_redis_alive():
         await msg.edit("`Database connections failing!`")
         return
-    textx = await msg.get_reply_message()
-    if textx:
-        try:
-            banreason = banreason.join(msg.text.split(" ")[1:])
-        except TypeError:
-            banreason = "[paperplane] FBan"
-    else:
-        banid = msg.text.split(" ")[1]
-        if banid.isnumeric():
-            # if its a user id
-            banid = int(banid)
-        else:
-            # deal wid the usernames
-            if msg.message.entities is not None:
-                probable_user_mention_entity = msg.message.entities[0]
 
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                banid = probable_user_mention_entity.user_id
-        try:
-            banreason = banreason.join(msg.text.split(" ")[2:])
-        except TypeError:
-            banreason = "[paperplane] FBan"
-    failed = {}
-    count = 1
-    fbanlist = []
-    x = await get_fban()
-    for i in x:
-        fbanlist.append(i["chatid"])
-    for bangroup in fbanlist:
+    banreason = "[paperplane] "
+
+    user, reason = await helpers.get_user_and_reason_from_event(msg)
+    if not user:
+        await msg.edit(
+            "`Missing user to FBan! Either reply to message, mention the user or specify a valid ID!`"
+        )
+        return
+    banid = user.id
+    banreason += reason if reason else "FBan"
+    message = ""
+
+    textx = await msg.get_reply_message()
+    if not textx:
+        message += "`Reply message missing! Might fail on some bots! Still attempting to FBan!`"
+        await msg.edit(message)
+
+    banlist = await get_fban()
+    count = 0
+    banid_list = [row["chatid"] for row in banlist]
+    for bangroup in banid_list:
         async with bot.conversation(bangroup) as conv:
             await conv.send_message(f"!fban {banid} {banreason}")
-            resp = await conv.get_response()
-            await bot.send_read_acknowledge(conv.chat_id)
-            if "Beginning federation ban " not in resp.text:
-                failed[bangroup] = str(conv.chat_id)
-            else:
-                count += 1
-                await msg.edit("`Fbanned on " + str(count) + " feds!`")
+            count += 1
             # Sleep to avoid a floodwait.
             # Prevents floodwait if user is a fedadmin on too many feds
-            await asyncio.sleep(0.2)
-    if failed:
-        failedstr = ""
-        for i in failed.values():
-            failedstr += i
-            failedstr += " "
-        await msg.reply(f"`Failed to fban in {failedstr}`")
-    else:
-        await msg.reply("`Fbanned in all feds!`")
+            await asyncio.sleep(0.1)
+
+    # We can't see if the user is actually FBanned.
+    message = f"\n`Fbanned on {str(count)} fed(s)! Paperplane doesn't check if the FBans were successful.`"
+    await msg.edit(message)
 
 
 @register(outgoing=True, pattern="^.addfban")
@@ -137,8 +104,14 @@ async def add_to_fban(chat):
     if not is_mongo_alive() or not is_redis_alive():
         await chat.edit("`Database connections failing!`")
         return
-    await add_chat_fban(chat.chat_id)
-    await chat.edit("`Added this chat under the Fbanlist!`")
+    res = await add_chat_fban(chat.chat_id)
+
+    if res:
+        return await chat.edit("`Added this chat to the FBan list!`")
+    else:
+        return await chat.edit(
+            "`Couldn't add this chat to the FBan list! Maybe it's already there?`"
+        )
 
 
 @register(outgoing=True, pattern="^.addgban")
@@ -147,9 +120,14 @@ async def add_to_gban(chat):
     if not is_mongo_alive() or not is_redis_alive():
         await chat.edit("`Database connections failing!`")
         return
-    await add_chat_gban(chat.chat_id)
-    print(chat.chat_id)
-    await chat.edit("`Added this bot under the Gbanlist!`")
+    res = await add_chat_gban(chat.chat_id)
+
+    if res:
+        return await chat.edit("`Added this bot to the GBan list!`")
+    else:
+        return await chat.edit(
+            "`Couldn't add this bot to the GBan list! Maybe it's already there?`"
+        )
 
 
 @register(outgoing=True, pattern="^.removefban")
@@ -158,8 +136,14 @@ async def remove_from_fban(chat):
     if not is_mongo_alive() or not is_redis_alive():
         await chat.edit("`Database connections failing!`")
         return
-    await remove_chat_fban(chat.chat_id)
-    await chat.edit("`Removed this chat from the Fbanlist!`")
+    res = await remove_chat_fban(chat.chat_id)
+
+    if res:
+        return await chat.edit("`Removed this group from the FBan list!`")
+    else:
+        return await chat.edit(
+            "`Couldn't remove this group from the FBan list! Maybe it's not there?`"
+        )
 
 
 @register(outgoing=True, pattern="^.removegban")
@@ -168,20 +152,68 @@ async def remove_from_gban(chat):
     if not is_mongo_alive() or not is_redis_alive():
         await chat.edit("`Database connections failing!`")
         return
-    await remove_chat_gban(chat.chat_id)
-    await chat.edit("`Removed this bot from the Gbanlist!`")
+    res = await remove_chat_gban(chat.chat_id)
+
+    if res:
+        return await chat.edit("`Removed this bot from the GBan list!`")
+    else:
+        return await chat.edit(
+            "`Couldn't remove this bot from the GBan list! Maybe it's not there?`"
+        )
+
+
+@register(outgoing=True, pattern="^.listfban")
+@grp_exclude()
+async def list_fban(chat):
+    if not is_mongo_alive() or not is_redis_alive():
+        await chat.edit("`Database connections failing!`")
+        return
+    banlist = await get_fban()
+    id_list = [row["chatid"] for row in banlist]
+    if len(id_list) < 1:
+        return await chat.edit(
+            "`No groups in the FBan list! Add some with the` `.addfban` `command!`"
+        )
+    message = "`Paperplane FBan groups`\n"
+
+    for id in id_list:
+        message += f"`* {id}`\n"
+
+    await chat.edit(message)
+
+
+@register(outgoing=True, pattern="^.listgban")
+@grp_exclude()
+async def list_gban(chat):
+    if not is_mongo_alive() or not is_redis_alive():
+        await chat.edit("`Database connections failing!`")
+        return
+    banlist = await get_fban()
+    id_list = [row["chatid"] for row in banlist]
+    if len(id_list) < 1:
+        return await chat.edit(
+            "`No bots in the GBan list! Add some with the` `.addgban` `command!`"
+        )
+    message = "`Paperplane GBan bots`\n"
+
+    for id in id_list:
+        message += f"`* {id}`\n"
+
+    await chat.edit(message)
 
 
 CMD_HELP.update(
     {
         "fbans/gbans": [
             "FBans/GBans",
-            " - `.gban`: Reply to a user to ban them in all the bots provided by you.\n"
-            " - `.fban`: Reply to a user to fban them in all the groups provided by you.\n"
-            " - `.addfban`: Add this group to the fbanlist.\n"
-            " - `.addgban`: Add this group to the gbanlist.\n"
-            " - `.removefban`: Remove this group from the fbanlist.\n"
-            " - `.removegban`: Remove this group from the gbanlist.\n",
+            " - `.gban`: Reply to a user to GBan them in all the bots provided by you.\n"
+            " - `.fban`: Reply to a user to FBan them in all the groups provided by you.\n"
+            " - `.addfban`: Add this group to the FBanlist.\n"
+            " - `.addgban`: Add this bot to the GBanlist.\n"
+            " - `.removefban`: Remove this group from the FBanlist.\n"
+            " - `.removegban`: Remove this bot from the GBanlist.\n",
+            " - `.listfban`: List all groups in the FBan list.\n",
+            " - `.listgban`: List all bots in the GBan list.\n",
         ]
     }
 )
---
+++ userbot/modules/helpers.py
@@ -0,0 +1,70 @@
+# Copyright (C) 2019-2021 The Authors
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+"""
+Helper functions to be used in modules
+"""
+
+from telethon.tl.types import MessageEntityMention, MessageEntityMentionName
+
+
+async def get_user_from_event(event):
+    """Get the user from argument or replied message."""
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.from_id)
+        return user_obj
+
+    user = event.pattern_match.group(1)
+    user_obj = None
+    if not user:
+        return False
+
+    if user.isnumeric():
+        user = int(user)
+
+    entity = user
+    if event.message.entities is not None:
+        (
+            prob_user_mention_entity,
+            prob_user_mention_text,
+        ) = event.message.get_entities_text()[0]
+
+        if isinstance(prob_user_mention_entity, MessageEntityMentionName):
+            entity = prob_user_mention_entity.user_id
+        elif isinstance(prob_user_mention_entity, MessageEntityMention):
+            entity = prob_user_mention_text
+
+    try:
+        user_obj = await event.client.get_entity(entity)
+    except (TypeError, ValueError):
+        return False
+
+    return user_obj
+
+
+async def get_user_and_reason_from_event(event):
+    user_obj = get_user_from_event()
+
+    if event.reply_to_msg_id:
+        reason = event.pattern_match.group(1)
+    else:
+        reason = event.pattern_match.group(2)
+
+    return (user_obj, reason)
+
+
+async def get_user_from_id(user, event):
+    """Getting user from user ID"""
+    if isinstance(user, str):
+        user = int(user)
+
+    try:
+        user_obj = await event.client.get_entity(user)
+    except (TypeError, ValueError) as err:
+        await event.edit(str(err))
+        return None
+
+    return user_obj
