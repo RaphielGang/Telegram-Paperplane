ff16ff4e25698afa0af26daab37c569878089795
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-07-19 17:14:58 +0530

    Update telegraph.py

--- userbot/modules/telegraph.py
+++ userbot/modules/telegraph.py
@@ -129,6 +129,10 @@ async def telegraph(text):
                 time.sleep(1)
                 Media = False
         else:
+            await text.edit("It's a non-textual file.")
+            time.sleep(1)
+            await text.edit("Lemme read the caption🔍")
+            time.sleep(1)
             Media = False
                                  
     elif not Media:
