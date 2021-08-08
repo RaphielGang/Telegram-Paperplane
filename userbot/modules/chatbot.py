9632df469505fbc1e7e044dbc6fc6936abd7a0e4
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-08-08 18:46:55 +0530

    Update chatbot.py

--- userbot/modules/chatbot.py
+++ userbot/modules/chatbot.py
@@ -74,8 +74,8 @@ async def chat_action(worker, response):
     if wait >= 15:
         wait = 15
     
+    await worker.client.action(worker.chat_id, 'cancel')
+    
     async with worker.client.action(worker.chat_id, 'typing'):
-        await worker.client.action(worker.chat_id, 'cancel')
         time.sleep(wait)
-        await worker.client.action(worker.chat_id, 'cancel')
 #   ......    #
