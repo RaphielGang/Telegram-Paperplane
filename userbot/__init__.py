8d098fd77779ec871cc243536e2a84f6ff023538
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2021-08-08 17:22:30 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -100,7 +100,8 @@ GDRIVE_FOLDER = os.environ.get("GDRIVE_FOLDER") or None
 HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY") or None
 HEROKU_APPNAME = os.environ.get("HEROKU_APPNAME") or None
 
-RANDOMSTUFF_API_KEY = os.environ.get("RANDOMSTUFF_API_KEY") or None
+BRAIN_ID = os.environ.get("BRAIN_ID") or None
+BRAIN_API_KEY = os.environ.get("BRAIN_API_KEY") or None
 
 WOLFRAM_ID = os.environ.get("WOLFRAM_ID") or None
 
