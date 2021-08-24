3506e7cfff54db3bdfb0acaa767488a632023509
Starry-Skies <tuturu528@gmail.com>
2021-08-25 00:36:00 +0530

    Typo fixed

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -29,7 +29,7 @@ from userbot import (
     PM_PASSWORD,
     is_mongo_alive,
     is_redis_alive,
-    
+
 )
 from userbot.events import register, grp_exclude
 from userbot.modules.dbhelper import (
@@ -51,7 +51,7 @@ UNAPPROVED_MSG_ON = PM_PERMIT_MSG or (
     "So I will wait for my owner to look in and approve you. They mostly approve PMs.\n\n"
     "**Till then, don't try to spam! Follow my warnings or I will block you!!**"
 )
-    
+
 UNAPPROVED_MSG_OFF = PM_PERMIT_MSG or (
     "Bleep blop! I am a bot.\n"
     "Hmm...I don't remember seeing you around here.\n\n"
@@ -59,7 +59,7 @@ UNAPPROVED_MSG_OFF = PM_PERMIT_MSG or (
     "My owner has turned off notifications, so I will read your messages and won't notify them.\n\n"
     "**Till then, don't try to spam! Follow my warnings or I will block you!!**"
 )
-        
+
 MAX_MSG = MAX_FLOOD_IN_PM or 5
 # =================================================================
 
@@ -74,8 +74,8 @@ async def iterate_delete(event, event_id, user, text):
         clean_msg = message.text
         if clean_msg == text:
             await message.delete()
-            
-            
+
+
 @register(incoming=True, disable_edited=True, disable_errors=True)
 @grp_exclude()
 async def permitpm(event):
@@ -105,44 +105,44 @@ async def permitpm(event):
                         if await notif_state() is False:
                             await event.respond(UNAPPROVED_MSG_OFF)
                             LASTMSG.update({event.chat_id: event.text})
-                        
+
                 if await notif_state() is False:
                     await event.client.send_read_acknowledge(event.chat_id)
                 if event.chat_id not in COUNT_PM:
                     COUNT_PM.update({event.chat_id: 1})
                 else:
                     COUNT_PM[event.chat_id] += 1
-                    
-                #=======================================   
+
+                #=======================================
                 WARN = MAX_MSG - COUNT_PM[event.chat_id] #
                 #=======================================
-                
-#==============#                                      
-                async def pm_notifier():        
+
+#==============#
+                async def pm_notifier():
                     if BOTLOG:
                         name = await event.client.get_entity(event.chat_id)
                         name0 = str(name.first_name)
-                        
+
                         log_message = (
                             "#INCOMING\n"
-                            + f"[{name0}](tg://user?id={event.chat_id})"                            
-                            + " is waiting in your PM.\n" 
-                            + f"[{name0}](tg://user?id={event.chat_id})" 
+                            + f"[{name0}](tg://user?id={event.chat_id})"
+                            + " is waiting in your PM.\n"
+                            + f"[{name0}](tg://user?id={event.chat_id})"
                             + " has sent {} message(s)."
                         )
-                        
+
                         if event.chat_id in COUNT_PM_LOG:
                             await COUNT_PM_LOG[event.chat_id].delete()
-                            
+
                         if WARN < 0:
                             return
                         else:
                             COUNT_PM_LOG[event.chat_id] = await event.client.send_message(
-                                    BOTLOG_CHATID, 
+                                    BOTLOG_CHATID,
                                     log_message.format(COUNT_PM[event.chat_id]))
-#==============#                                      
-                                                        
-    
+#==============#
+
+
                 if WARN > 1:
                     message = await event.reply(f"You have {WARN} warns left.")
                     await delete_in(message, 5)
@@ -160,11 +160,11 @@ async def permitpm(event):
                                     "So now you are BLOCKED and REPORTED spam!!")
                     await iterate_delete(event, event.chat_id, "me", UNAPPROVED_MSG_ON)
                     await iterate_delete(event, event.chat_id, "me", UNAPPROVED_MSG_OFF)
-                
+
                 if WARN <= -1:
                     await event.client(BlockRequest(event.chat_id))
                     await event.client(ReportSpamRequest(peer=event.chat_id))
-                    
+
                     try:
                         del COUNT_PM[event.chat_id]
                         del LASTMSG[event.chat_id]
@@ -177,7 +177,7 @@ async def permitpm(event):
                                 )
                         LOGS.info("PMPermit broke, please restart Paperplane.")
                         return
-                
+
                     if BOTLOG:
                         name = await event.client.get_entity(event.chat_id)
                         name0 = str(name.first_name)
@@ -190,7 +190,7 @@ async def permitpm(event):
                             + ")"
                             + " was spamming your PM and has been blocked.",
                         )
-                    
+
 
 
 # Some fun func:
@@ -204,47 +204,47 @@ async def pm_password(event):
                 limit=300
             ):
                 password = password.text
-                
+
                 if password == PM_PASSWORD:
                     await approve(event.chat_id)
                     await iterate_delete(event, event.chat_id, "me", UNAPPROVED_MSG_ON)
                     await iterate_delete(event, event.chat_id, "me", UNAPPROVED_MSG_OFF)
                     await iterate_delete(event, event.chat_id, event.chat_id, PM_PASSWORD)
-                    
+
                     wlcm_msg = (
-                        await event.reply("Welcome, I am a bot!!\n" 
+                        await event.reply("Welcome, I am a bot!!\n"
                                       "Very nice to meet you😊 "
                                       "I will ping my owner about you now."
                                      )
                     )
-                    
+
                     async with event.client.conversation(event.chat_id) as conv:
                         rmd_msg = await conv.send_message(
                             "Just say a \"Hi\"."
                         )
                         response = (await conv.get_response()).message
-                        
+
                         if response == ("Hi" or "\"Hi\""):
                             message = await event.reply(
                                 "Great! Now you can be comfortable, I won't intuerrupt😉"
                             )
                         else:
                             message = await event.reply(
-                                "Ok it wasn't a \"Hi\"," 
+                                "Ok it wasn't a \"Hi\","
                                 " but I won't interrupt anymore. Take care☺️"
                             )
-                            
+
                         if await conv.wait_read(message):
                             await delete_in(wlcm_msg, 0)
                             await delete_in(rmd_msg, 0)
                             await delete_in(message, 3)
-                    
+
                     try:
                         del COUNT_PM[event.chat_id]
                         del LASTMSG[event.chat_id]
                     except KeyError:
                         pass
-                    
+
                     if BOTLOG:
                         name = await event.client.get_entity(event.chat_id)
                         name0 = str(name.first_name)
@@ -254,10 +254,10 @@ async def pm_password(event):
                                                         "entered PM password and has been approved."
                                                        )
                     return
-                    
+
 #
- 
-    
+
+
 @register(outgoing=True, pattern="(autoapprove|autoa)$")
 @grp_exclude()
 async def auto_approve_switch(event):
@@ -267,15 +267,15 @@ async def auto_approve_switch(event):
         return
     else:
         await autoapprove()
-        
+
     if await autoapproval() is True:
         x = await event.edit("`Auto-Approve` ON! I will approve an user after you PM them.")
         await delete_in(x, 5)
     else:
         y = await event.edit("`Auto-Approve` OFF! You will have to manually approve users now.")
         await delete_in(y, 5)
-    
-    
+
+
 @register(disable_edited=True, outgoing=True, disable_errors=True)
 @grp_exclude()
 async def auto_accept(event):
@@ -284,10 +284,10 @@ async def auto_accept(event):
         chat = await event.get_chat()
         if not is_mongo_alive() or not is_redis_alive():
             return
-        if event.text.startswith((".block", ".autoa", ".a", ".da", ".autoapprove", 
+        if event.text.startswith((".block", ".autoa", ".a", ".da", ".autoapprove",
                                   ".approve", ".disapprove", ".notifon", ".notifoff")):
             return
-        if event.is_private: 
+        if event.is_private:
             if not await approval(chat.id) and not chat.bot:
                 await approve(chat.id)
                 await iterate_delete(event, chat.id, "me", UNAPPROVED_MSG_ON)
@@ -299,7 +299,7 @@ async def auto_accept(event):
                     "#AUTO_APPROVED\n"
                     + "User: "
                     + f"[{chat.first_name}](tg://user?id={chat.id})",
-              )              
+              )
 
 @register(outgoing=True, pattern="notifoff$")
 @grp_exclude()
@@ -311,8 +311,8 @@ async def notifoff(noff_event):
         return await delete_in(x, 5)
     else:
         y = await noff_event.edit("Notifications silenced!")
-        return await delete_in(y, 5)  
- 
+        return await delete_in(y, 5)
+
 
 @register(outgoing=True, pattern="notifon$")
 @grp_exclude()
@@ -324,9 +324,9 @@ async def notifon(non_event):
     else:
         y = await non_event.edit("Notifications unmuted!")
         return await delete_in(y, 5)
-    
-    
-#=============================================== 
+
+
+#===============================================
 # Now here's the approve, disapprove, block and unblock command helper.
 #===================================
 
@@ -366,7 +366,7 @@ async def error_handler(event):
         message = "Well, I can't unblock __nobody__"
     await event.edit(message)
     return await delete_in(message, 5)
-        
+
 """Exeption Handler"""
 async def exception_handler(event, type):
     if type == 'username':
@@ -382,9 +382,9 @@ async def exception_handler(event, type):
         return await delete_in(message, 5)
 #===================================
 # Here it ends.
-#=============================================== 
+#===============================================
 
-    
+
 @register(outgoing=True, pattern="(?:approve|a)(?: |$)(.*)$")
 @grp_exclude()
 async def approvepm(apprvpm):
@@ -392,29 +392,29 @@ async def approvepm(apprvpm):
     if not is_mongo_alive() or not is_redis_alive():
         await apprvpm.edit("Database connections failing!")
         return
-    
+
     try:
         aname, uid = await user_getter(apprvpm)
     except:
         return
-    
+
     if await approval(uid) is True:
         x = await apprvpm.edit("I already know this user! You can chat!")
         return await delete_in(x, 5)
-    
+
     await approve(uid)
     await apprvpm.edit(f"I will remember [{aname}](tg://user?id={uid}) as your __mutual__ contact😉")
     await asyncio.sleep(3)
     await iterate_delete(apprvpm, uid, "me", UNAPPROVED_MSG_ON)
     await iterate_delete(apprvpm, uid, "me", UNAPPROVED_MSG_OFF)
-    
+
     if apprvpm.pattern_match.group(1):
         await apprvpm.edit(f"[{aname}](tg://user?id={uid}) can PM you now.")
     elif apprvpm.reply_to_msg_id:
         await apprvpm.edit(f"[{aname}](tg://user?id={uid}), you can PM without an issue😊")
     elif apprvpm.is_private:
         await apprvpm.edit("Hey there! Nice to meet you😊 I am an obidient bot of my owner!")
-        
+
     if uid in LASTMSG:
         del LASTMSG[uid]
     if uid in COUNT_PM:
@@ -424,9 +424,9 @@ async def approvepm(apprvpm):
         await apprvpm.client.send_message(
             BOTLOG_CHATID, "#APPROVED\n" + "User: " + f"[{aname}](tg://user?id={uid})"
         )
-        
-        
-        
+
+
+
 @register(outgoing=True, pattern="(?:disapprove|da)(?: |$)(.*)$")
 @grp_exclude()
 async def dapprovepm(dapprvpm):
@@ -434,16 +434,16 @@ async def dapprovepm(dapprvpm):
     if not is_mongo_alive() or not is_redis_alive():
         await dapprvpm.edit("Database connections failing!")
         return
-    
+
     try:
         dname, uid = await user_getter(dapprvpm)
     except:
         return
-    
+
     if await approval(uid) is False:
         x = await dapprvpm.edit("The user is already a stranger for me.")
         return await delete_in(x, 5)
-    
+
     await disapprove(uid)
     await dapprvpm.edit(f"Forgetting [{dname}](tg://user?id={uid}) .")
     await asyncio.sleep(1)
@@ -453,10 +453,10 @@ async def dapprovepm(dapprvpm):
     await asyncio.sleep(1)
     await dapprvpm.edit(f"Forgetting [{dname}](tg://user?id={uid}) ... Done!")
     await asyncio.sleep(1)
-    
+
     if PM_PASSWORD:
         await iterate_delete(dapprvpm, uid, uid, PM_PASSWORD)
-    
+
     if dapprvpm.pattern_match.group(1):
         await dapprvpm.edit(f"I will guard your PM from [{dname}](tg://user?id={uid}).")
     elif dapprvpm.is_private:
@@ -469,14 +469,14 @@ async def dapprovepm(dapprvpm):
             BOTLOG_CHATID, "#DISAPPROVED\n" + "User: " + f"[{dname}](tg://user?id={uid})"
         )
 
-        
+
 @register(outgoing=True, pattern="block(?: |$)(.*)$")
 @grp_exclude()
 async def blockpm(block):
     """You can block some unwanted people."""
-    
+
     try:
-        uname, uid = await user_getter(block)
+        bname, uid = await user_getter(block)
     except:
         return
     
@@ -486,28 +486,28 @@ async def blockpm(block):
         await block.edit("I am blocking you now.")
     elif block.reply_to_msg_id:
         await block.edit("You won't look good in PM.")
-        
+
     await block.client(BlockRequest(uid))
     await block.edit("***Blocked***")
-    
+
     if BOTLOG:
         await block.client.send_message(
-            BOTLOG_CHATID, "#BLOCKED\n" + "User: " + f"[{name0}](tg://user?id={uid})"
+            BOTLOG_CHATID, "#BLOCKED\n" + "User: " + f"[{bname}](tg://user?id={uid})"
         )
 
-        
+
 
 @register(outgoing=True, pattern="unblock(?: |$)(.*)$")
 @grp_exclude()
 async def unblockpm(unblock):
-    """You can unblock the people 
+    """You can unblock the people
             so that they can PM again."""
-    
+
     try:
         uname, uid = await user_getter(unblock)
     except:
         return
-    
+
     if unblock.pattern_match.group(1):
         await unblock.edit(f"I will unblock {uname} in three swconds but are you sure?")
     elif unblock.is_private:
@@ -515,20 +515,20 @@ async def unblockpm(unblock):
         return await delete_in(x, 5)
     elif unblock.reply_to_msg_id:
         await unblock.edit("You are gonna get unblocked, aren't you happy?")
-    
+
     time.sleep(2)
-    await unblock.client(UnblocRequest(uid))
+    await unblock.client(UnblockRequest(uid))
     time.sleep(1)
     await unblock.edit("Let's make peace.")
-    
+
     if BOTLOG:
         await unblock.client.send_message(
             BOTLOG_CHATID,
             f"#UNBLOCKED\n[{uname}](tg://user?id={uid}) was unblocked!.",
         )
 
-        
-        
+
+
 
 CMD_HELP.update(
     {
