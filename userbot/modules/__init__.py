bc18c2dc32319b2f5c043d674f2b4241fd529325
Ryo <89829215+Ryoishin@users.noreply.github.com>
2021-09-01 12:51:38 +0530

    Update __init__.py

--- userbot/modules/__init__.py
+++ userbot/modules/__init__.py
@@ -12,12 +12,11 @@ def __list_all_modules():
     import glob
 
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
-    all_modules = [
+    return = [
         basename(f)[:-3]
         for f in mod_paths
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
-    return all_modules
 
 
 ALL_MODULES = sorted(__list_all_modules())
